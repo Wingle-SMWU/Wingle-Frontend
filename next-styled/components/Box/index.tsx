c90e57d527d141be46ba7543ebd27bd283832e4c
ZUITOPIA <korhwngml123@naver.com>
2023-01-13 16:31:51 +0900

    초기화

--- next-styled/components/Box/index.tsx
+++
@@ -1,5 +0,0 @@
-import { BoxProps } from "shared/const";
-
-const Box = ({ name }: BoxProps) => <div>{name}</div>;
-
-export default Box;
--- next-styled/hooks/useTitle.js
+++
@@ -1,9 +0,0 @@
-export const useTitle = initialTitle => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
