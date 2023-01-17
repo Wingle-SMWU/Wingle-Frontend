88509b3542ec20ac8d1cd4cf3b837dff6b6dd90a
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2023-01-17 22:37:48 +0900

    Feature/4 community design (#6)
    * feat: text guide에 따른 ui 세팅 수정
    
    * 브랜치
    
    * Community List Icon, Image 저장
    
    * Community List UI 추가
    
    * package-lock.json 파일 변경
    
    Co-authored-by: ZUITOPIA <korhwngml123@naver.com>
    Co-authored-by: Juhee Ko <70759627+ZUITOPIA@users.noreply.github.com>

--- wingle/src/components/layout/Footer/index.tsx
+++ wingle/src/components/layout/Footer/index.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Margin, Text } from "../../ui";
 
 const Style = {
   Wrapper: styled.div`
@@ -7,12 +8,35 @@ const Style = {
     background-color: #fcfcfc;
     border-top: 1px solid #eeeef2;
   `,
+  Content: styled.div`
+    padding: 32px 24px;
+  `,
+  WingleLogo: styled.img`
+    width: 80px;
+    height: 30px;
+  `,
 };
 
 export default function Footer() {
   return (
     <>
-      <Style.Wrapper>얍얍</Style.Wrapper>
+      <Style.Wrapper>
+        <Style.Content>
+          <Style.WingleLogo src="footer-logo.png" alt="wingle-logo" />
+          <Margin direction="column" size={32} />
+          <Text.Caption2 color="gray500">
+            공식 노션 바로가기 | 윙글 SNS 바로가기
+          </Text.Caption2>
+          <Margin direction="column" size={11} />
+          <Text.Caption2 color="gray500">
+            문의 | wingle.kr@gmail.com
+          </Text.Caption2>
+          <Margin direction="column" size={11} />
+          <Text.Caption2 color="gray500">
+            이용약관 | 개인정보처리방침
+          </Text.Caption2>
+        </Style.Content>
+      </Style.Wrapper>
     </>
   );
 }
