5c5bb3b06602b3ad10930e47f9ccce28f59ae0fe
ZUITOPIA <korhwngml123@naver.com>
2023-01-17 15:18:15 +0900

    feat: 전체 크기 재조정

--- wingle/src/components/layout/Footer/index.tsx
+++ wingle/src/components/layout/Footer/index.tsx
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
+const Style = {
+  Wrapper: styled.div`
+    width: 500px;
+    height: 270px;
+    background-color: #fcfcfc;
+    border-top: 1px solid #eeeef2;
+  `,
+};
+
 export default function Footer() {
-  return <>푸터</>;
+  return (
+    <>
+      <Style.Wrapper>얍얍</Style.Wrapper>
+    </>
+  );
 }
--- wingle/src/pages/_app.tsx
+++ wingle/src/pages/_app.tsx
@@ -4,16 +4,28 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global-style";
 import { theme } from "../styles/theme";
 
+import styled from "styled-components";
+const Style = {
+  Wrapper: styled.div`
+    width: 500px;
+    height: 951px;
+    background-color: white;
+  `,
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>boilerplate</title>
+        <title>국제교류 온라인 플랫폼, Wingle</title>
+        <link rel="icon" href="/logo_favicon.jpeg" />
       </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Style.Wrapper>
+          <Component {...pageProps} />
+        </Style.Wrapper>
       </ThemeProvider>
     </>
   );
