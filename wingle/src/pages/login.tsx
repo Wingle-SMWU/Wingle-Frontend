2c3befa5814b7f6fde6b21404f584acfc13abe9d
chan <75971206+chan1009@users.noreply.github.com>
2023-01-17 18:53:11 +0900

    design: login, register 페이지 UI 추가

--- wingle/src/pages/login.tsx
+++ wingle/src/pages/login.tsx
@@ -1,7 +1,59 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import styled from "styled-components";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
   return (
     <>
-      <div>로그인 페이지</div>
+      <Header>
+        <img src="wingle.png" alt="logo" />
+        <Text>다 함께 즐기는 국제 교류 커퓨니티</Text>
+      </Header>
+      <Wrapper>
+        <InputGroup placeholder="이메일" value={email} />
+        <InputGroup placeholder="비밀번호" value={email} />
+      </Wrapper>
+      <Wrapper>
+        <LoginButton>로그인</LoginButton>
+        <RegisterButton>회원가입</RegisterButton>
+      </Wrapper>
     </>
   );
 }
+
+const Header = styled.div`
+  width: 200px;
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 48px;
+`;
+
+const Text = styled.div`
+  padding-top: 15px;
+`;
+
+const Wrapper = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 48px;
+`;
+
+const InputGroup = styled.input`
+  width: 452px;
+  height: 50px;
+  border-radius: 8px;
+  border: 1px solid #dcdce0;
+  margin-bottom: 20px;
+`;
+
+const LoginButton = styled.button`
+  width: 452px;
+  height: 50px;
+  background-color: #eeeef2;
+`;
+const RegisterButton = styled.button`
+  width: 50px;
+  margin: 20px;
+  border-bottom: 1px solid black;
+`;
--- wingle/src/pages/register.tsx
+++ wingle/src/pages/register.tsx
@@ -1,7 +1,58 @@
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global-style";
+import { theme } from "../styles/theme";
+import React, { FormEvent, useState } from "react";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({});
+
   return (
     <>
-      <div>회원가입 페이지</div>
+      <Header>
+        <Image>
+          <img src="back.png" alt="back" />
+        </Image>
+        <Text>회원가입</Text>
+      </Header>
+      <Container>
+        <Title>학생증 인증</Title>
+        <Icon>
+          <img src="question.png" alt="question" />
+        </Icon>
+      </Container>
     </>
   );
 }
+
+const Header = styled.div`
+  text-align: left;
+  display: flex;
+  padding-bottom: 20px;
+`;
+
+const Image = styled.button`
+  padding: 20px;
+`;
+
+const Text = styled.h1`
+  padding-top: 23px;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+`;
+
+const Title = styled.h1`
+  font-size: 25px;
+  font-weight: 700;
+`;
+
+const Icon = styled.button`
+  padding: 5px;
+`;
