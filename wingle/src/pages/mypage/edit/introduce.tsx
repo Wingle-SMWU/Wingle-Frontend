6e826720a1503bfde303bd2da171ccd0cbd1ccaf
ZUITOPIA <korhwngml123@naver.com>
2023-01-19 16:57:35 +0900

    feat: 자기소개 textarea 추가

--- wingle/src/pages/mypage/edit/introduce.tsx
+++ wingle/src/pages/mypage/edit/introduce.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import router from "next/router";
 import { Text } from "@/src/components/ui";
+import { useState } from "react";
 
 const Style = {
   Wapper: styled.div`
@@ -25,9 +26,24 @@ const Style = {
     margin-right: 12px;
     cursor: pointer;
   `,
+
+  Description: styled.textarea`
+    width: 100%;
+    min-height: 500px;
+    padding: 30px 0px;
+    border: 1px solid white;
+    font-size: 16px;
+    color: #222223;
+    outline: none;
+    resize: none;
+    ::placeholder {
+      font-weight: 400;
+      color: gray;
+    }
+  `,
 };
 
-export default function introduce() {
+export default function Introduce() {
   return (
     <>
       <Style.Wapper>
@@ -46,11 +62,17 @@ export default function introduce() {
             <Text.Body1
               color="gray900"
               onClick={() => router.push(`/mypage/edit`)}
+              // onClick={handleSubmit} 얘도 추가해야함
               pointer
             >
               완료
             </Text.Body1>
           </Style.Header>
+
+          <Style.Description
+            maxLength={400}
+            placeholder="자기소개를 작성해주세요! (최대 400자)"
+          />
         </Style.Content>
       </Style.Wapper>
     </>
