2aa156b7fe94093732eddee8ffc294d072c6402c
sehee-xx <didtpgml0627@gmail.com>
2023-01-20 15:47:20 +0900

    community list header tab border-bottom 추가

--- wingle/src/components/community/list/header.tsx
+++ wingle/src/components/community/list/header.tsx
@@ -14,21 +14,70 @@ const Style = {
     flex-direction: row;
     align-items: center;
     border-bottom: 1px solid #eeeef2;
-    padding: 8px 24px;
+    padding: 8px 24px 0px 24px;
     gap: 24px;
+  `,
+
+  TextUnderLineOrange: styled.div`
+    border-bottom: 2px solid #ff812e;
+    padding-bottom: 6px;
+  `,
+
+  TextUnderLineNone: styled.div`
+    border-bottom: 2px solid #fff;
+    padding-bottom: 6px;
   `,
 };
 
-export default function Header() {
+export default function Header(props: {
+  tab: string;
+  onChangeTab: (event: any) => void;
+}) {
   return (
     <>
       <Style.Header>
         <Text.Title2 color="gray900">커뮤니티</Text.Title2>
       </Style.Header>
       <Style.HeaderBar>
-        <Text.Title3 color="gray900">자유</Text.Title3>
-        <Text.Title3 color="gray500">교류</Text.Title3>
-        <Text.Title3 color="gray500">공지</Text.Title3>
+        {props.tab === "자유" ? (
+          <Style.TextUnderLineOrange>
+            <Text.Title3 color="gray900" onClick={props.onChangeTab}>
+              자유
+            </Text.Title3>
+          </Style.TextUnderLineOrange>
+        ) : (
+          <Style.TextUnderLineNone>
+            <Text.Title3 color="gray500" onClick={props.onChangeTab}>
+              자유
+            </Text.Title3>
+          </Style.TextUnderLineNone>
+        )}
+        {props.tab === "교류" ? (
+          <Style.TextUnderLineOrange>
+            <Text.Title3 color="gray900" onClick={props.onChangeTab}>
+              교류
+            </Text.Title3>
+          </Style.TextUnderLineOrange>
+        ) : (
+          <Style.TextUnderLineNone>
+            <Text.Title3 color="gray500" onClick={props.onChangeTab}>
+              교류
+            </Text.Title3>
+          </Style.TextUnderLineNone>
+        )}
+        {props.tab === "공지" ? (
+          <Style.TextUnderLineOrange>
+            <Text.Title3 color="gray900" onClick={props.onChangeTab}>
+              공지
+            </Text.Title3>
+          </Style.TextUnderLineOrange>
+        ) : (
+          <Style.TextUnderLineNone>
+            <Text.Title3 color="gray500" onClick={props.onChangeTab}>
+              공지
+            </Text.Title3>
+          </Style.TextUnderLineNone>
+        )}
       </Style.HeaderBar>
     </>
   );
