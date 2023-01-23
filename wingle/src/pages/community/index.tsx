e079a2944ae66a615b9b13ed8bc0536ad3d62c2c
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2023-01-23 22:26:46 +0900

    Feature/9 community create (#12)
    * 현재 tab create 페이지로 전달 기능 추가
    
    * community create UI 작성
    
    * Modal component 추가
    
    * 사용하지 않는 import 삭제
    
    * community create UI 수정 및 router.query로 tab 정보 연결

--- wingle/src/pages/community/index.tsx
+++ wingle/src/pages/community/index.tsx
@@ -4,48 +4,57 @@ import Header from "@/src/components/community/list/header";
 import InteractTab from "@/src/components/community/list/interactTab";
 import NoticeTab from "@/src/components/community/list/noticeTab";
 import { useRouter } from "next/router";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import styled from "styled-components";
+
+type Tab = {
+  tab: string;
+};
 
 export default function Community() {
   const router = useRouter();
 
-  const [tab, setTab] = useState("자유");
+  const currentTab: string = useMemo(() => {
+    if (!router.query.tab) {
+      return "자유";
+    }
+    return String(router.query.tab);
+  }, [router.query.tab]);
 
   const onClickMoveToWrite = () => {
-    router.push("/community/create");
+    router.push({ pathname: `/community/create`, query: { tab: currentTab } });
   };
 
   const onClickTab = (event: any) => {
-    setTab(event.target.textContent);
+    router.push({ query: { tab: event.target.textContent } });
   };
 
   const imgUrl: string = useMemo(() => {
-    if (tab === "자유") {
+    if (currentTab === "자유") {
       return "community/list/wingle-default.svg";
     }
-    if (tab === "교류") {
+    if (currentTab === "교류") {
       return "community/list/list_profile_flag.png";
     }
-    if (tab === "공지") {
+    if (currentTab === "공지") {
       return "community/list/wingle-manager.svg";
     }
     return "";
-  }, [tab]);
+  }, [currentTab]);
 
   return (
     <>
-      <Header tab={tab} onClickTab={onClickTab}></Header>
+      <Header tab={currentTab} onClickTab={onClickTab}></Header>
       <Style.Body>
-        <Style.CreateAbsolutePoint>
+        <Style.CreateAbsolutePoint tab={currentTab}>
           <Style.CreateIcon
             src="community/list/new_write.svg"
             onClick={onClickMoveToWrite}
           />
         </Style.CreateAbsolutePoint>
-        {tab === "교류" && <InteractTab imgUrl={imgUrl}></InteractTab>}
-        {tab === "공지" && <NoticeTab imgUrl={imgUrl}></NoticeTab>}
-        {tab === "자유" && <FreeTab imgUrl={imgUrl}></FreeTab>}
+        {currentTab === "교류" && <InteractTab imgUrl={imgUrl}></InteractTab>}
+        {currentTab === "공지" && <NoticeTab imgUrl={imgUrl}></NoticeTab>}
+        {currentTab === "자유" && <FreeTab imgUrl={imgUrl}></FreeTab>}
       </Style.Body>
       <Footer></Footer>
     </>
@@ -61,8 +70,9 @@ const Style = {
     background-color: white;
   `,
 
-  CreateAbsolutePoint: styled.div`
+  CreateAbsolutePoint: styled.div<Tab>`
     position: absolute;
+    display: ${({ tab }) => (tab === "공지" ? "none" : "block")};
   `,
 
   CreateIcon: styled.img`
