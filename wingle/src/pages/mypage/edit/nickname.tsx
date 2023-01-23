4c5049a4be7b5e602cc5076273a4729a4511aa5b
ZUITOPIA <korhwngml123@naver.com>
2023-01-23 20:24:28 +0900

    feat: 프로필수정 - 닉네임, 이미지 수정 ui 완료

--- wingle/src/pages/mypage/edit/nickname.tsx
+++ wingle/src/pages/mypage/edit/nickname.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import router from "next/router";
-import { Text } from "@/src/components/ui";
+import { Margin, Text } from "@/src/components/ui";
 
 const Style = {
   Wapper: styled.div`
@@ -13,6 +13,18 @@ const Style = {
   `,
   Content: styled.div`
     padding: 24px;
+  `,
+  ImageChangeBox: styled.div`
+    width: 88px;
+    height: 88px;
+    margin: 30px 0px;
+  `,
+  NicknameChangeBox: styled.div``,
+  InputNickname: styled.input`
+    width: 100%;
+    height: 52px;
+    border: 1px solid #dcdce0;
+    border-radius: 8px;
   `,
   Header: styled.div`
     width: 100%;
@@ -44,13 +56,27 @@ export default function Nickname() {
               <Text.Title1 color="gray900">프로필 수정</Text.Title1>
             </Style.Left>
             <Text.Body1
-              color="gray900"
+              color="gray500" // 비활성화 상태
+              // 활성화 상태에서는 color="gray900"
               onClick={() => router.push(`/mypage/edit`)}
               pointer
             >
               완료
             </Text.Body1>
           </Style.Header>
+          <>
+            <Style.ImageChangeBox>이미지</Style.ImageChangeBox>
+
+            <Style.NicknameChangeBox>
+              <Text.Body5 color="gray700">닉네임</Text.Body5>{" "}
+              <Margin direction="column" size={8} />
+              <Style.InputNickname />
+              <Margin direction="column" size={8} />
+              <Text.Caption3 color="gray900">
+                한글/영어 2글자 이상 10글자 이하
+              </Text.Caption3>
+            </Style.NicknameChangeBox>
+          </>
         </Style.Content>
       </Style.Wapper>
     </>
