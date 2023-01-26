32532aa677e70b6fe5067fae6eb17876747fad3b
sehee-xx <didtpgml0627@gmail.com>
2023-01-26 22:56:04 +0900

    Navigation 추가

---
+++ wingle/src/components/layout/Navigation/index.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { Text } from "../../ui";
+
+const NavigationMenuArr = [
+  {
+    name: "커뮤니티",
+    page: "/community",
+    normalImg: "/community/list/comu-normal.svg",
+    disableImg: "/community/list/comu-disable.svg",
+  },
+  {
+    name: "쪽지",
+    page: "/message",
+    normalImg: "/community/list/message-normal.svg",
+    disableImg: "/community/list/message-disable.svg",
+  },
+  {
+    name: "마이페이지",
+    page: "/mypage",
+    normalImg: "/community/list/wingle-manager.svg",
+    disableImg: "/community/list/mypage-disable.svg",
+  },
+];
+
+export default function Navigation() {
+  const router = useRouter();
+  const menu = router.asPath;
+
+  return (
+    <Style.Wrapper>
+      {NavigationMenuArr.map((el) => (
+        <Style.NavigationMenu key={el.name} href={el.page}>
+          <Style.NavigationMenuImg
+            src={menu.includes(el.page) ? el.normalImg : el.disableImg}
+          />
+          <Text.Caption2 color={menu.includes(el.page) ? "gray900" : "gray500"}>
+            {el.name}
+          </Text.Caption2>
+        </Style.NavigationMenu>
+      ))}
+    </Style.Wrapper>
+  );
+}
+
+const Style = {
+  Wrapper: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border-top: 1px solid #eeeef2;
+  `,
+
+  NavigationMenu: styled(Link)`
+    width: 104px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 12.5px 24px;
+    text-decoration: none;
+  `,
+
+  NavigationMenuImg: styled.img``,
+};
