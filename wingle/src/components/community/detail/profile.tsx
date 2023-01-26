5dc3536e073c7d811642c25e60323accecf1e171
sehee-xx <didtpgml0627@gmail.com>
2023-01-26 22:54:04 +0900

    community detail 컴포넌트 분리

---
+++ wingle/src/components/community/detail/profile.tsx
@@ -0,0 +1,66 @@
+import { getImageUrl } from "@/src/modules/utils";
+import { useState } from "react";
+import styled from "styled-components";
+import Modal from "../../modal";
+import { Margin, Text } from "../../ui";
+
+export default function Profile(props: { currentTab: string }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const onClickModal = () => {
+    setModalVisible((prev) => !prev);
+  };
+
+  return (
+    <>
+      <Style.Profile>
+        <Style.ProfileLeft>
+          <Style.ProfileImg src={getImageUrl(props.currentTab)} />
+          <Margin direction="row" size={10} />
+          <Style.ProfileInfo>
+            <Text.Body6 color="gray900">한국윙그리</Text.Body6>
+            <Text.Caption3 color="gray500">10분 전</Text.Caption3>
+          </Style.ProfileInfo>
+        </Style.ProfileLeft>
+        <Style.CancelImg
+          src="/community/detail/close-gray.svg"
+          onClick={onClickModal}
+        />
+      </Style.Profile>
+      {modalVisible && (
+        <Modal type="detail-delete-contents" onClickModal={onClickModal} />
+      )}
+    </>
+  );
+}
+
+const Style = {
+  Profile: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 65px 24px 8px 24px;
+  `,
+
+  ProfileImg: styled.img`
+    border-radius: 50%;
+    width: 36px;
+    height: 36px;
+  `,
+
+  ProfileInfo: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+
+  ProfileLeft: styled.div`
+    display: flex;
+    flex-direction: row;
+  `,
+
+  CancelImg: styled.img`
+    width: 24px;
+    height: 24px;
+  `,
+};
