ef101cf4a2c29552df52082975c93558954f8f37
sehee-xx <didtpgml0627@gmail.com>
2023-01-26 22:54:35 +0900

    주석 삭제

--- wingle/src/components/community/list/freeTab.tsx
+++ wingle/src/components/community/list/freeTab.tsx
@@ -1,6 +1,5 @@
-// 자유 탭
 import ListCard from "./listCard";
 
 export default function FreeTab(props: { imgUrl: string }) {
-  return <ListCard imgUrl={props.imgUrl}></ListCard>;
+  return <ListCard imgUrl={props.imgUrl} isNotice={false}></ListCard>;
 }
--- wingle/src/components/community/list/noticeTab.tsx
+++ wingle/src/components/community/list/noticeTab.tsx
@@ -1,6 +1,5 @@
-// 공지 탭
 import ListCard from "./listCard";
 
 export default function NoticeTab(props: { imgUrl: string }) {
-  return <ListCard imgUrl={props.imgUrl}></ListCard>;
+  return <ListCard imgUrl={props.imgUrl} isNotice={true}></ListCard>;
 }
