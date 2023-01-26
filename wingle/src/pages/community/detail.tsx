5589f41399a85ae081de2fe2da7d41a3f1e397c7
sehee-xx <didtpgml0627@gmail.com>
2023-01-26 22:57:41 +0900

    community detail UI 작성

--- wingle/src/pages/community/detail.tsx
+++ wingle/src/pages/community/detail.tsx
@@ -1,7 +1,35 @@
+import Body from "@/src/components/community/detail/body";
+import Comment from "@/src/components/community/detail/comment";
+import CommentInput from "@/src/components/community/detail/commentInput";
+import Header from "@/src/components/community/detail/header";
+import Profile from "@/src/components/community/detail/profile";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+
 export default function Detail() {
+  const router = useRouter();
+  const currentTab = `${router.query.tab}`;
+
   return (
-    <>
-      <div>커뮤니티 글 상세페이지</div>
-    </>
+    <Style.Wrapper>
+      <Style.DetailTop>
+        <Header currentTab={currentTab} />
+        <Profile currentTab={currentTab} />
+        <Body />
+        <Comment currentTab={currentTab} />
+      </Style.DetailTop>
+      <CommentInput />
+    </Style.Wrapper>
   );
 }
+
+const Style = {
+  Wrapper: styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  `,
+
+  DetailTop: styled.div``,
+};
