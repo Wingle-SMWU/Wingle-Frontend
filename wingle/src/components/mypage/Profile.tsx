f72c66a2223a6cc51777790988d54dcd9bf87edd
ZUITOPIA <korhwngml123@naver.com>
2023-01-31 13:38:40 +0900

    feat: 프로필 재사용 가능하도록

--- wingle/src/components/mypage/Profile.tsx
+++ wingle/src/components/mypage/Profile.tsx
@@ -4,135 +4,83 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 const Style = {
-  UserBox: styled.div`
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #eeeef2;
-    gap: 14px;
-    position: relative;
-  `,
-  UserImgBox: styled.div`
-    width: 56px;
-    height: 56px;
-    border: 1px solid green;
-  `,
-  UserProfileImg: styled.img`
-    border: 1px solid #eeeef2;
-  `,
-  UserFlagImg: styled.img``,
-  UserInfoBox: styled.div`
-    width: 340px;
-    height: 86px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  `,
-  UserNicknameAndSex: styled.div`
-    display: flex;
-  `,
-  UserSexImg: styled.img``,
-  RegisterBtn: styled.button`
-    width: 45px;
-    height: 33px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #ff812e;
-    border-radius: 8px;
-  `,
-  EditBtn: styled.button`
-    width: 45px;
-    height: 33px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid #6c6c70;
-    border-radius: 8px;
-  `,
-  DropBubbleHigh: styled.div`
-    position: absolute;
-    top: 63px;
-    left: 435px;
-    border-bottom: 8px solid #303033;
-    border-left: 4px solid transparent;
-    border-right: 4px solid transparent;
-  `,
+    UserBox: styled.div`
+        display: flex;
+        align-items: center;
+        /* border-bottom: 1px solid #eeeef2; */
+        gap: 14px;
+        position: relative;
+    `,
+    UserImgBox: styled.div`
+        width: 56px;
+        height: 56px;
+        border: 1px solid green;
+    `,
+    UserProfileImg: styled.img`
+        border: 1px solid #eeeef2;
+    `,
+    UserFlagImg: styled.img``,
+    UserInfoBox: styled.div`
+        width: 340px;
+        height: 86px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    `,
+    UserNicknameAndSex: styled.div`
+        display: flex;
+    `,
+    UserSexImg: styled.img``,
 
-  DropBubbleLow: styled.div`
-    width: 153px;
-    height: 42px;
-    background-color: #303033;
-    border-radius: 8px;
-    position: absolute;
-    top: 70px;
-    left: 300px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `,
+    DropBubbleHigh: styled.div`
+        position: absolute;
+        top: 63px;
+        left: 435px;
+        border-bottom: 8px solid #303033;
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+    `,
+
+    DropBubbleLow: styled.div`
+        width: 153px;
+        height: 42px;
+        background-color: #303033;
+        border-radius: 8px;
+        position: absolute;
+        top: 70px;
+        left: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `,
 };
 
 export default function Profile() {
-  const router = useRouter();
-  const [isRegisterBtnHover, setIsRegisterBtnHover] = useState(false);
+    const router = useRouter();
+    const [isRegisterBtnHover, setIsRegisterBtnHover] = useState(false);
 
-  const handleRegisterBtnHover = () => {
-    setIsRegisterBtnHover(true);
-  };
-  const handleRegisterBtnLeave = () => {
-    setIsRegisterBtnHover(false);
-  };
-  return (
-    <>
-      <Style.UserBox>
-        <Style.UserImgBox>
-          <Style.UserProfileImg src="" alt="프로필" />
-          {/* <Style.UserFlagImg src="" alt="국기" /> */}
-        </Style.UserImgBox>
-        <Style.UserInfoBox>
-          <Style.UserNicknameAndSex>
-            <Text.Body1 color="gray900">닉네임</Text.Body1>
-            {/* {gender ? 여자 이미지 : 남자 이미지} */}
-            <Style.UserSexImg src="" alt="성별" />
-          </Style.UserNicknameAndSex>
-          <Text.Body6 color="gray800">국적</Text.Body6>
-        </Style.UserInfoBox>
-
-        {/* 자기소개, 언어선택, 관심사 중 하나라도 등록되지 않은 사용자 ? 등록 : 수정*/}
-
-        {/* <>
-
-        <Style.RegisterBtn
-          onMouseOver={() => setIsRegisterBtnHover(true)}
-          onMouseLeave={() => setIsRegisterBtnHover(false)}
-        >
-          <Text.Caption1
-            color="white"
-            pointer
-            onClick={() => router.push(`/mypage/profileEdit`)}
-          >
-            등록
-          </Text.Caption1>
-        </Style.RegisterBtn>
-        {isRegisterBtnHover && (
-          <>
-            <Style.DropBubbleHigh />
-            <Style.DropBubbleLow>
-              <Text.Body6>프로필을 등록해주세요!</Text.Body6>
-            </Style.DropBubbleLow>
-          </>
-        )}
-        </> */}
-        <Style.EditBtn>
-          <Text.Caption1
-            color="gray700"
-            pointer
-            onClick={() => router.push(`/mypage/edit`)}
-          >
-            수정
-          </Text.Caption1>
-        </Style.EditBtn>
-      </Style.UserBox>
-    </>
-  );
+    const handleRegisterBtnHover = () => {
+        setIsRegisterBtnHover(true);
+    };
+    const handleRegisterBtnLeave = () => {
+        setIsRegisterBtnHover(false);
+    };
+    return (
+        <>
+            <Style.UserBox>
+                <Style.UserImgBox>
+                    <Style.UserProfileImg src="" alt="프로필" />
+                    {/* <Style.UserFlagImg src="" alt="국기" /> */}
+                </Style.UserImgBox>
+                <Style.UserInfoBox>
+                    <Style.UserNicknameAndSex>
+                        <Text.Body1 color="gray900">닉네임</Text.Body1>
+                        {/* {gender ? 여자 이미지 : 남자 이미지} */}
+                        <Style.UserSexImg src="" alt="성별" />
+                    </Style.UserNicknameAndSex>
+                    <Text.Body6 color="gray800">국적</Text.Body6>
+                </Style.UserInfoBox>
+            </Style.UserBox>
+        </>
+    );
 }
--- wingle/src/pages/mypage/edit/index.tsx
+++ wingle/src/pages/mypage/edit/index.tsx
@@ -1,181 +1,170 @@
 import Modal from "@/src/components/modal";
+import Profile from "@/src/components/mypage/Profile";
 import { Text, Margin } from "@/src/components/ui";
 import router from "next/router";
 import { useState } from "react";
 import styled from "styled-components";
 
 const Style = {
-  Wapper: styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    /* border: 1px solid red; */
-  `,
-  Content: styled.div`
-    padding: 24px;
-  `,
-  Header: styled.div`
-    width: 100%;
-    height: 56px;
-  `,
-  GoBackArrow: styled.img`
-    margin-right: 12px;
-    cursor: pointer;
-  `,
+    Wapper: styled.div`
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        /* border: 1px solid red; */
+    `,
+    Content: styled.div`
+        padding: 24px;
+    `,
+    Header: styled.div`
+        width: 100%;
+        height: 56px;
+    `,
+    GoBackArrow: styled.img`
+        margin-right: 12px;
+        cursor: pointer;
+    `,
 
-  UserBox: styled.div`
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #eeeef2;
-    gap: 14px;
-    position: relative;
-  `,
-  UserImgBox: styled.div`
-    width: 56px;
-    height: 56px;
-    border: 1px solid green;
-  `,
-  UserProfileImg: styled.img`
-    border: 1px solid #eeeef2;
-  `,
-  UserFlagImg: styled.img``,
-  UserInfoBox: styled.div`
-    width: 340px;
-    height: 86px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  `,
-  UserNicknameAndSex: styled.div`
-    display: flex;
-  `,
-  UserSexImg: styled.img``,
-  RegisterBtn: styled.button`
-    width: 45px;
-    height: 33px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #ff812e;
-    border-radius: 8px;
-  `,
-  EditBtn: styled.img`
-    width: 24px;
-    height: 24px;
-    border-radius: 18px;
-    cursor: pointer;
-  `,
-  EditList: styled.div`
-    width: 452px;
-    display: flex;
-    flex-direction: column;
-  `,
-  Language: styled.div`
-    width: 452px;
-    display: flex;
-    justify-content: space-between;
-    /* border: 1px solid blue; */
-  `,
-  Introduce: styled.div`
-    width: 452px;
-    display: flex;
-    justify-content: space-between;
-    /* border: 1px solid blue; */
-  `,
-  Interest: styled.div`
-    width: 452px;
-    display: flex;
-    justify-content: space-between;
-    /* border: 1px solid blue; */
-  `,
+    UserBox: styled.div`
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #eeeef2;
+        gap: 14px;
+        position: relative;
+    `,
+    UserImgBox: styled.div`
+        width: 56px;
+        height: 56px;
+        border: 1px solid green;
+    `,
+    UserProfileImg: styled.img`
+        border: 1px solid #eeeef2;
+    `,
+    UserFlagImg: styled.img``,
+    UserInfoBox: styled.div`
+        width: 340px;
+        height: 86px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    `,
+    UserNicknameAndSex: styled.div`
+        display: flex;
+    `,
+    UserSexImg: styled.img``,
+    RegisterBtn: styled.button`
+        width: 45px;
+        height: 33px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #ff812e;
+        border-radius: 8px;
+    `,
+    EditBtn: styled.img`
+        width: 24px;
+        height: 24px;
+        border-radius: 18px;
+        cursor: pointer;
+    `,
+    EditList: styled.div`
+        width: 452px;
+        display: flex;
+        flex-direction: column;
+    `,
+    Language: styled.div`
+        width: 452px;
+        display: flex;
+        justify-content: space-between;
+        /* border: 1px solid blue; */
+    `,
+    Introduce: styled.div`
+        width: 452px;
+        display: flex;
+        justify-content: space-between;
+        /* border: 1px solid blue; */
+    `,
+    Interest: styled.div`
+        width: 452px;
+        display: flex;
+        justify-content: space-between;
+        /* border: 1px solid blue; */
+    `,
 };
 export default function Edit() {
-  const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
-  const onClickModal = () => {
-    setModalVisible((prev) => !prev);
-  };
-  return (
-    <>
-      <Style.Wapper>
-        <Style.Content>
-          <Style.Header>
-            <Style.GoBackArrow
-              src="/back-arrow.svg"
-              alt="뒤로가기"
-              onClick={() => router.push(`/mypage`)}
-            />
-            <Text.Title1 color="gray900">프로필 수정</Text.Title1>
-          </Style.Header>
-          <>
-            <Style.UserBox>
-              <Style.UserImgBox>
-                <Style.UserProfileImg src="" alt="프로필" />
-                {/* <Style.UserFlagImg src="" alt="국기" /> */}
-              </Style.UserImgBox>
-              <Style.UserInfoBox>
-                <Style.UserNicknameAndSex>
-                  <Text.Body1 color="gray900">닉네임</Text.Body1>
-                  <Style.UserSexImg src="" alt="성별" />
-                </Style.UserNicknameAndSex>
-                <Text.Body6 color="gray800">국적</Text.Body6>
-              </Style.UserInfoBox>
+    const onClickModal = () => {
+        setModalVisible(prev => !prev);
+    };
+    return (
+        <>
+            <Style.Wapper>
+                <Style.Content>
+                    <Style.Header>
+                        <Style.GoBackArrow
+                            src="/back-arrow.svg"
+                            alt="뒤로가기"
+                            onClick={() => router.push(`/mypage`)}
+                        />
+                        <Text.Title1 color="gray900">프로필 수정</Text.Title1>
+                    </Style.Header>
+                    <>
+                        <Style.UserBox>
+                            <Profile />
 
-              <Style.EditBtn
-                src="/modify.svg"
-                alt="연필"
-                onClick={() => router.push(`/mypage/edit/nickname`)}
-              />
-            </Style.UserBox>
-          </>
+                            <Style.EditBtn
+                                src="/modify.svg"
+                                alt="연필"
+                                onClick={() => router.push(`/mypage/edit/nickname`)}
+                            />
+                        </Style.UserBox>
+                    </>
 
-          <Style.EditList>
-            <Margin direction="column" size={32} />
-            <Style.Language>
-              <Text.Body1 color="gray900" pointer>
-                사용 가능 언어
-              </Text.Body1>
-              <Style.EditBtn
-                src="/modify.svg"
-                alt="연필"
-                onClick={() => router.push(`/mypage/edit/language`)}
-              />
-              {/* 두번째 연필 누르면 "사용 가능 언어" 페이지로 가서 1순위, 2순위, 3순위 언어선택 */}
-            </Style.Language>
+                    <Style.EditList>
+                        <Margin direction="column" size={32} />
+                        <Style.Language>
+                            <Text.Body1 color="gray900" pointer>
+                                사용 가능 언어
+                            </Text.Body1>
+                            <Style.EditBtn
+                                src="/modify.svg"
+                                alt="연필"
+                                onClick={() => router.push(`/mypage/edit/language`)}
+                            />
+                            {/* 두번째 연필 누르면 "사용 가능 언어" 페이지로 가서 1순위, 2순위, 3순위 언어선택 */}
+                        </Style.Language>
 
-            <Margin direction="column" size={32} />
-            <Style.Introduce>
-              <Text.Body1 color="gray900" pointer>
-                자기소개
-              </Text.Body1>
-              <Style.EditBtn
-                src="/modify.svg"
-                alt="연필"
-                onClick={() => router.push(`/mypage/edit/introduce`)}
-              />
-              {/* 세번째 연필 누르면 "자기소개" 페이지로 가서 자기소개 글쓰기 */}
-            </Style.Introduce>
+                        <Margin direction="column" size={32} />
+                        <Style.Introduce>
+                            <Text.Body1 color="gray900" pointer>
+                                자기소개
+                            </Text.Body1>
+                            <Style.EditBtn
+                                src="/modify.svg"
+                                alt="연필"
+                                onClick={() => router.push(`/mypage/edit/introduce`)}
+                            />
+                            {/* 세번째 연필 누르면 "자기소개" 페이지로 가서 자기소개 글쓰기 */}
+                        </Style.Introduce>
 
-            <Margin direction="column" size={32} />
-            <Style.Interest>
-              <Text.Body1 color="gray900" pointer>
-                관심사
-              </Text.Body1>
-              <Style.EditBtn
-                src="/modify.svg"
-                alt="연필"
-                onClick={() => router.push(`/mypage/edit/interest`)}
-              />
-              {/* 네번째 연필 누르면 "관심사" 페이지로 가서 관심사 선택하기 */}
-            </Style.Interest>
-          </Style.EditList>
-        </Style.Content>
-        {modalVisible && (
-          <Modal type="profile-back" onClickModal={onClickModal} />
-        )}
-      </Style.Wapper>
-    </>
-  );
+                        <Margin direction="column" size={32} />
+                        <Style.Interest>
+                            <Text.Body1 color="gray900" pointer>
+                                관심사
+                            </Text.Body1>
+                            <Style.EditBtn
+                                src="/modify.svg"
+                                alt="연필"
+                                onClick={() => router.push(`/mypage/edit/interest`)}
+                            />
+                            {/* 네번째 연필 누르면 "관심사" 페이지로 가서 관심사 선택하기 */}
+                        </Style.Interest>
+                    </Style.EditList>
+                </Style.Content>
+                {modalVisible && <Modal type="profile-back" onClickModal={onClickModal} />}
+            </Style.Wapper>
+        </>
+    );
 }
