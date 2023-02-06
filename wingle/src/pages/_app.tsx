1d148c0ea28d42c9cfa6811256131e5f9774c82a
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2023-02-07 00:14:02 +0900

    Body width, height 수정 (#18)

--- wingle/src/pages/_app.tsx
+++ wingle/src/pages/_app.tsx
@@ -7,8 +7,9 @@ import { theme } from "../styles/theme";
 import styled from "styled-components";
 const Style = {
   Wrapper: styled.div`
-    width: 500px;
-    height: 951px;
+    width: 100%;
+    max-width: 500px;
+    height: 100vh;
     background-color: white;
   `,
 };
