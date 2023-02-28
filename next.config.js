3aafde26acc27b962651aed819bc7f9f3719ced2
yeonkr <1714882@sookmyung.ac.kr>
2023-02-28 17:26:17 +0900

    fix: next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+module.exports = nextConfig;
