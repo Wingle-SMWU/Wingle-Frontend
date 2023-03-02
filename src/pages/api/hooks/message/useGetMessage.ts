edd44a64dcdedc2e83bf26807c4f8721cec90e4a
yeonkr <1714882@sookmyung.ac.kr>
2023-03-02 18:01:39 +0900

    feat: useGetMessage hook 추가

---
+++ src/pages/api/hooks/message/useGetMessage.ts
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import useMsgAPI from "./useMsgAPI";
+
+interface User {
+  image: string;
+  nickname: string;
+  memberId: number;
+}
+
+const useGetMessage = (roomId: number) => {
+  const { getRoomAllMessage } = useMsgAPI();
+
+  const [roomList, setRoomList] = useState<any>([]); // 화면
+  const [messageList, setMessageList] = useState<any[]>([]);
+  const [myInfo, setMyInfo] = useState<User>();
+  const [receiverInfo, setReceiverInfo] = useState<User>();
+
+  const getmessageData = async () => {
+    try {
+      return getRoomAllMessage(roomId);
+    } catch (data: any) {
+      setRoomList(data);
+      setMessageList(data);
+      data.users.map((user: any) => {
+        if (user) {
+          setMyInfo(user);
+        } else {
+          setReceiverInfo(user);
+        }
+      });
+    }
+  };
+  getmessageData();
+
+  return {
+    roomList,
+    messageList,
+    setMessageList,
+    myInfo,
+    receiverInfo,
+  };
+};
+
+export default useGetMessage;
