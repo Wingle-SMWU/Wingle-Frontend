8a1f936596b8b28eaa6b6aaa8b93e62ec6ea9acb
Rhino-ty <okko8522@gmail.com>
2023-04-06 16:27:32 +0900

    feat: form 태그 추가 및 POST할 데이터 타입화 및 네트워크 호출
    임시적으로 작성했고, 지금 여러 컴포넌트로 나눠져 있어 리코일을 사용해 제출이나 체크할 때마다 아톰 업데이트 해야할듯

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -9,6 +9,106 @@ import GenderSelectBox from "../../components/authpage/signup/genderSelect";
 import AgreeBox from "@/src/components/authpage/signup/agreeBox";
 import router from "next/router";
 import Image from "next/image";
+import { useMutation } from "react-query";
+import axios from "axios";
+
+interface SdInputProps {
+  complete: boolean;
+}
+
+interface SignUpData {
+  idCardImage: string;
+  email: string;
+  password: string;
+  name: string;
+  isNicknameChecked: boolean;
+  nickname: string;
+  gender: boolean;
+  nation: string;
+  termsOfUse: boolean;
+  termsOfPersonalInformation: boolean;
+  termsOfPromotion: boolean;
+}
+
+export default function SignUp() {
+  useEffect(() => {
+    handleComplete();
+  });
+
+  const [error, setError] = useState<Boolean>(false);
+  const [check, setCheck] = useState<Boolean>(false);
+  const [complete, setComplete] = useState(false);
+
+  const getError = (error: Boolean) => {
+    setError(error);
+  };
+
+  const getCheck = (check: Boolean) => {
+    setCheck(check);
+  };
+
+  const handleComplete = () => {
+    if (error === false && check === true) {
+      setComplete(true);
+    } else {
+      setComplete(false);
+    }
+  };
+
+  const signUpMutation = useMutation((signUpData: SignUpData) =>
+    axios.post("/api/signup", signUpData)
+  );
+
+  const handleSignUpSubmit = () => {
+    // TODO: 제출
+    // if (complete) {
+    //   signUpMutation.mutate({
+    //     idCardImage: "image",
+    //     email: "wingle@gmail.com",
+    //     password: "1234!",
+    //     name: "김윙글",
+    //     isNicknameChecked: true,
+    //     nickname: "윙그리",
+    //     gender: true,
+    //     nation: "kr",
+    //     termsOfUse: true,
+    //     termsOfPersonalInformation: true,
+    //     termsOfPromotion: false,
+    //   });
+    // }
+  };
+
+  return (
+    <S.Wrapper>
+      <S.HeaderWrapper>
+        <S.BackButton
+          src="/auth/arrow_back.svg"
+          alt="arrow"
+          width={24}
+          height={24}
+          onClick={() => router.push("/auth/login")}
+        />
+        <Margin direction="row" size={14} />
+        <Text.Title2 color="gray900">회원가입</Text.Title2>
+      </S.HeaderWrapper>
+      <form onSubmit={handleSignUpSubmit}>
+        <StudentCard />
+        <Text.Title1 color="gray900">학생 정보</Text.Title1>
+        <Margin direction="column" size={16} />
+        <InputBox getError={getError} />
+        <DropDown />
+        <GenderSelectBox />
+        <AgreeBox getCheck={getCheck} />
+        <S.CompleteButton
+          complete={complete}
+          onClick={() => (complete ? router.replace("complete") : console.log("disabled"))}
+        >
+          <Text.Body1 color={complete ? "white" : "gray500"}>작성완료</Text.Body1>
+        </S.CompleteButton>
+      </form>
+    </S.Wrapper>
+  );
+}
 
 const S = {
   Wrapper: styled.div`
@@ -30,59 +130,3 @@ const S = {
     margin-bottom: 333px;
   `,
 };
-
-interface SdInputProps {
-  complete: boolean;
-}
-
-export default function SignUp() {
-  useEffect(() => {
-    handleComplete();
-  });
-  const [error, setError] = useState(false);
-  const [check, setCheck] = useState(false);
-  const [complete, setComplete] = useState(false);
-  const getError = (error: any) => {
-    setError(error);
-  };
-  const getCheck = (check: any) => {
-    setCheck(check);
-  };
-  const handleComplete = () => {
-    if (error === false && check === true) {
-      setComplete(true);
-    } else {
-      setComplete(false);
-    }
-  };
-  return (
-    <>
-      <S.Wrapper>
-        <S.HeaderWrapper>
-          <S.BackButton
-            src="/auth/arrow_back.svg"
-            alt="arrow"
-            width={24}
-            height={24}
-            onClick={() => router.push("/auth/login")}
-          />
-          <Margin direction="row" size={14} />
-          <Text.Title2 color="gray900">회원가입</Text.Title2>
-        </S.HeaderWrapper>
-        <StudentCard />
-        <Text.Title1 color="gray900">학생 정보</Text.Title1>
-        <Margin direction="column" size={16} />
-        <InputBox getError={getError} />
-        <DropDown />
-        <GenderSelectBox />
-        <AgreeBox getCheck={getCheck} />
-        <S.CompleteButton
-          complete={complete}
-          onClick={() => (complete ? router.replace("complete") : console.log("disabled"))}
-        >
-          <Text.Body1 color={complete ? "white" : "gray500"}>작성완료</Text.Body1>
-        </S.CompleteButton>
-      </S.Wrapper>
-    </>
-  );
-}
