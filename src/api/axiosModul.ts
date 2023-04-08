6ac4aa66355234c01615bbab4dc55d2a345668af
Rhino-ty <okko8522@gmail.com>
2023-04-08 12:06:50 +0900

    feat: axios 모듈로 만들어서 통일
    hooks에 있던 SERVER_URL 옮겨서 instance로 만들어 통일, example 만들어서 예시 제작

---
+++ src/api/axiosModul.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const SERVER_URL = `https://server-prod.wingle.kr/api/v1`;
+
+const instance = axios.create({
+  baseURL: SERVER_URL,
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYWJAYWEuY29tIiwiQVVUSCI6IlJPTEVfVVNFUiIsImV4cCI6MjAwMzM4OTE0MH0.mD1FqQkgMTKZB-Pkwd1qWIWMK4tl_hGInEDG1aP7rB7ZZOJIlj6AmlCxJvwJSXTtui7vl3bpyMLf__v_gJyTnA`,
+  },
+});
+
+export default instance;
--- src/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export const SERVER_URL = `https://server-prod.wingle.kr/api/v1`;
