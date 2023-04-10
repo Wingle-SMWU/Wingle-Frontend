28c9c117e82bd3e879d52be7b9ea4a4b9a6cfd10
jaehoon <halmokme@gmail.com>
2023-04-10 13:10:47 +0900

    feat: header ui

---
+++ src/components/admin/header.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+export default function Header() {
+  return (
+    <S.Header>
+      <S.Logo>
+        <Image src='admin/logo.svg' alt='logo' width={28} height={28} />
+      </S.Logo>
+      <S.Title>Wingle Admin</S.Title>
+    </S.Header>
+  )
+}
+
+const S = {
+  Header: styled.div`
+    width: 1440px;
+    height: 64px;
+    background: #ffffff;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  `,
+  Logo: styled.div`
+    position: absolute;
+    width: 28px;
+    height: 28px;
+    left: 277px;
+    top: 18px;
+  `,
+  Title: styled.div`
+    position: absolute;
+    width: 105px;
+    height: 22px;
+    left: 317px;
+    top: 21px;
+    width: 105px;
+    height: 22px;
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 22px;
+  `
+}
