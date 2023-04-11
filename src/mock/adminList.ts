f050c9b5fad5e170955592260c7c67be45c1179e
jaehoon <halmokme@gmail.com>
2023-04-12 01:16:32 +0900

    chore: mock data 수정

--- src/mock/adminList.ts
+++ src/mock/adminList.ts
@@ -4,87 +4,9 @@ export const adminList = [
     "message": "수락 완료 목록 조회 성공",
     "data": [
         {
-            "userId": 100,
-            "createdTime": "2023-02-21T10:28:57.157627",
-            "name": "이름1",
-            "nation": "KR"
-        },
-        {
-            "userId": 101,
-            "createdTime": "2023-02-22T10:28:33.475461",
-            "name": "이름2",
-            "nation": null
-        },
-        {
-            "userId": 102,
-            "createdTime": "2023-02-23T10:28:33.475461",
-            "name": "이름3",
-            "nation": "EN"
-        },
-        {
-            "userId": 103,
-            "createdTime": "2023-02-24T10:28:33.475461",
-            "name": "이름4",
-            "nation": "EN"
-        },
-        {
-            "userId": 104,
-            "createdTime": "2023-02-25T10:28:33.475461",
-            "name": "이름5",
-            "nation": "KR"
-        },
-        {
-            "userId": 105,
-            "createdTime": "2023-02-26T10:28:33.475461",
-            "name": "이름6",
-            "nation": "KR"
-        },
-        {
-            "userId": 106,
-            "createdTime": "2023-02-27T10:28:33.475461",
-            "name": "이름7",
-            "nation": "KR"
-        },
-        {
-            "userId": 107,
-            "createdTime": "2023-02-28T10:28:33.475461",
-            "name": "이름8",
-            "nation": "KR"
-        },
-        {
-            "userId": 108,
-            "createdTime": "2023-03-01T10:28:33.475461",
-            "name": "이름9",
-            "nation": "EU"
-        },
-        {
-            "userId": 109,
-            "createdTime": "2023-03-01T10:50:33.475461",
-            "name": "이름10",
-            "nation": "EU"
-        },
-        {
-            "userId": 110,
-            "createdTime": "2023-03-02T10:28:33.475461",
-            "name": "이름11",
-            "nation": "EU"
-        },
-        {
-            "userId": 111,
-            "createdTime": "2023-03-03T10:28:33.475461",
-            "name": "이름12",
-            "nation": "EU"
-        },
-        {
-            "userId": 112,
-            "createdTime": "2023-03-04T10:28:33.475461",
-            "name": "이름13",
-            "nation": "EN"
-        },
-        {
-            "userId": 113,
-            "createdTime": "2023-03-05T10:28:33.475461",
-            "name": "이름14",
+            "userId": 115,
+            "createdTime": "2023-03-07T10:28:33.475461",
+            "name": "이름16",
             "nation": "KR"
         },
         {
@@ -94,10 +16,84 @@ export const adminList = [
             "nation": "KR"
         },
         {
-            "userId": 115,
-            "createdTime": "2023-03-07T10:28:33.475461",
-            "name": "이름16",
+            "userId": 113,
+            "createdTime": "2023-03-05T10:28:33.475461",
+            "name": "이름14",
             "nation": "KR"
+        },
+        {
+            "userId": 112,
+            "createdTime": "2023-03-04T10:28:33.475461",
+            "name": "이름13",
+            "nation": "EN"
+        },
+        {
+            "userId": 111,
+            "createdTime": "2023-03-03T10:28:33.475461",
+            "name": "이름12",
+            "nation": "EU"
+        },
+        {
+            "userId": 110,
+            "createdTime": "2023-03-02T10:28:33.475461",
+            "name": "이름11",
+            "nation": "EU"
+        },
+        {
+            "userId": 109,
+            "createdTime": "2023-03-01T10:50:33.475461",
+            "name": "이름10",
+            "nation": "EU"
+        },
+        {
+            "userId": 108,
+            "createdTime": "2023-03-01T10:28:33.475461",
+            "name": "이름9",
+            "nation": "EU"
+        },
+        {
+            "userId": 107,
+            "createdTime": "2023-02-28T10:28:33.475461",
+            "name": "이름8",
+            "nation": "KR"
+        },
+        {
+            "userId": 106,
+            "createdTime": "2023-02-27T10:28:33.475461",
+            "name": "이름7",
+            "nation": "KR"
+        },
+        {
+            "userId": 105,
+            "createdTime": "2023-02-26T10:28:33.475461",
+            "name": "이름6",
+            "nation": "KR"
+        },
+        {
+            "userId": 104,
+            "createdTime": "2023-02-25T10:28:33.475461",
+            "name": "이름5",
+            "nation": "KR"
+        },
+        {
+            "userId": 103,
+            "createdTime": "2023-02-24T10:28:33.475461",
+            "name": "이름4",
+            "nation": "EN"
+        },
+
+        {
+            "userId": 102,
+            "createdTime": "2023-02-23T10:28:33.475461",
+            "name": "이름3",
+            "nation": "EN"
+        },
+
+        {
+            "userId": 101,
+            "createdTime": "2023-02-22T10:28:33.475461",
+            "name": "이름2",
+            "nation": null
         },
     ]
 }
