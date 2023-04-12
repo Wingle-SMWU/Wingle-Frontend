3574851cb47b7164d91ae02bc2268639e10e8623
김유나 <yuna000804@gmail.com>
2023-04-13 02:39:35 +0900

    #40 - enhance: instance interceptor 추가

--- src/api/axiosModule.ts
+++ src/api/axiosModule.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 export const SERVER_URL = `https://server-prod.wingle.kr/api/v1`;
 
@@ -8,5 +8,13 @@ const instance = axios.create({
     Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYWJAYWEuY29tIiwiQVVUSCI6IlJPTEVfVVNFUiIsImV4cCI6MjAwMzM4OTE0MH0.mD1FqQkgMTKZB-Pkwd1qWIWMK4tl_hGInEDG1aP7rB7ZZOJIlj6AmlCxJvwJSXTtui7vl3bpyMLf__v_gJyTnA`,
   },
 });
+
+instance.interceptors.response.use((response: AxiosResponse):Promise<AxiosResponse> => {
+  const { status, data } = response;
+  if(status !== 200) {
+    return Promise.reject(response);
+  }
+  return data;
+})
 
 export default instance;
