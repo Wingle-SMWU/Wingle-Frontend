247724cbfb2b8251ce00478d957de98107c3926c
jaehoon <halmokme@gmail.com>
2023-04-13 02:26:31 +0900

    feat: 유저승인, 거절 api 로직 분리

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -2,17 +2,14 @@ import instance from './axiosModul'
 
 type GetList = {
   path: string;
-  page: number;
-}
-
-type GetUser = {
-  path: string;
-  userId: string | string[] | undefined;
+  page?: number;
+  userId?: string | string[] | undefined;
 }
 
 type PostPermission = {
   path: string;
   userId: string | undefined;
+  reason?: string;
 }
 
 export const adminListAPI = {
@@ -23,7 +20,7 @@ export const adminListAPI = {
     return res.data;
   },
 
-  getUser: async ({ path, userId }: GetUser) => {
+  getUser: async ({ path, userId }: GetList) => {
     const res = await instance.get(`${adminListAPI.url}/${path}/${userId}`);
     return res.data;
   } 
@@ -32,8 +29,18 @@ export const adminListAPI = {
 export const adminPerAPI = {
   url: '/admin/permission',
 
-  postAccept: async({ path, userId }: PostPermission) => {
-    const res = await instance.post(`${adminPerAPI.url}/${path}`, {userId});
+  // postAccept: async({ path, userId }: PostPermission) => {
+  //   const res = await instance.post(`${adminPerAPI.url}/${path}`, {userId});
+  //   return res.data;
+  // },
+
+  // postReject: async({ path, userId, reason}: PostPermission) => {
+  //   const res = await instance.post(`${adminPerAPI.url}/${path}`, {userId, reason});
+  //   return res.data;
+  // },
+
+  post: async({ path, userId, reason }: PostPermission) => {
+    const res = await instance.post(`${adminPerAPI.url}/${path}`, {userId, reason});
     return res.data;
   }
 }
