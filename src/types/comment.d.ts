c0ff9b6bdf1e7877e2c1772bc6c8f97f8daaf04e
김유나 <yuna000804@gmail.com>
2023-04-13 05:08:10 +0900

    #40 - fix: type 파일 types 폴더로 이동

---
+++ src/types/comment.d.ts
@@ -0,0 +1,11 @@
+interface Comment {
+  content: string,
+  createdTime: string,
+  updatedTime: string,
+  id: number,
+  isMine: boolean,
+  userId: number,
+  userImage: string,
+  userNation: string,
+  userNickname: string,
+}
