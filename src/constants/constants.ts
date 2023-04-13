74c82ae3367ccffc06d72271a6be9190ec82e39e
jaehoon <halmokme@gmail.com>
2023-04-13 16:21:10 +0900

    feat: 임시저장 커스텀훅 생성

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,10 @@
 export const USER_NATIONALITY: string[] = ['한국인', '외국인'];
+import { ReactNode } from 'react';
 
 export const ADMIN_TAB_LIST: string[] = ['수락대기', '수락완료', '수락거절'];
 
 export const ADMIN_CONTENT_MENU: string[] = ['No', 'Name', '구분'];
 
 export const ADMIN_GET_LIST: string[] = ['waiting', 'acceptance', 'rejection'];
+
+export const ADMIN_TEMP_STORE = { 거절사유: 'rejection', 메모: 'memo' } as const;
---
+++ src/hooks/admin/usePostTemp.ts
@@ -0,0 +1,19 @@
+import { useMutation } from 'react-query';
+import { adminTempAPI } from '../../api/admin';
+import { ReactNode } from 'react';
+import { ADMIN_TEMP_STORE } from '@/src/constants/constants';
+
+export default function usePostTemp(children: ReactNode, userId: string | undefined, val: string) {
+
+  const path = ADMIN_TEMP_STORE[children];
+  const body = { userId };
+  body[ADMIN_TEMP_STORE[children]] = val;
+
+
+  const { mutate, isLoading, error } = useMutation(() => adminTempAPI.post(path, body), {
+    onSuccess: (res) => console.log(res),
+    onError: (res) => console.log(res),
+  })
+
+  return { mutate, isLoading, error };
+}
