1e5d0a580ee88e72ed8c64d2c6a10f2f9a7f0019
Rhino-ty <okko8522@gmail.com>
2023-04-20 22:18:26 +0900

    #35 - fix: API 주석 해제 및 formData에 append 불리언 따로 구현

--- src/api/auth/signUpApi.ts
+++ src/api/auth/signUpApi.ts
@@ -14,20 +14,23 @@ export interface SignUpData {
   termsOfPromotion: boolean;
 }
 
-export const postSignUp = async (signUpData: SignUpData) => {
+const withFormData = (data: SignUpData) => {
   // multipart/form-data 형식으로 데이터를 보내기 위해 FormData 객체를 생성
-  // const formData = new FormData();
-  // Object.keys(signUpData).forEach((key) => {
-  //   // idCardImage를 제외한 나머지 필드는 직접 formData에 추가
-  //   if (key !== "idCardImage") {
-  //     formData.append(key, signUpData[key]);
-  //   }
-  // });
-  // formData.append("idCardImage", signUpData.idCardImage);
-  // const response = await instance.post("/auth/signup", formData, {
-  //   headers: {
-  //     "Content-Type": "multipart/form-data",
-  //   },
-  // });
-  // return response.data;
+  const formData = new FormData();
+  Object.entries(data).forEach(([key, value]) => {
+    if (typeof value === "boolean") {
+      formData.append(key, value ? "true" : "false");
+    } else {
+      formData.append(key, value);
+    }
+  });
+  return formData;
+};
+
+export const postSignUp = async (signUpData: SignUpData) => {
+  const response = await instance.post("/signup", withFormData(signUpData), {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
+  return response.data;
 };
