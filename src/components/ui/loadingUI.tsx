3407e74d423224b46192113e5eb9473818640b31
김유나 <yuna000804@gmail.com>
2023-05-02 20:58:52 +0900

    style: 로딩 페이지 크기 및 위치 조정

--- src/components/ui/loadingUI.tsx
+++ src/components/ui/loadingUI.tsx
@@ -1,40 +1,39 @@
-import React from 'react';
-import styled,{keyframes} from 'styled-components';
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const Loading = () => {
-    const text = "Loading"
-    return (
-        <S.SpinnerWrapper>
-            <S.Spinner /> 
-            {/* {text && <S.SpinnerText>{text}</S.SpinnerText>} */}
-        </S.SpinnerWrapper>
-    );
+  const text = "Loading";
+  return (
+    <S.SpinnerWrapper>
+      <S.Spinner />
+      {/* {text && <S.SpinnerText>{text}</S.SpinnerText>} */}
+    </S.SpinnerWrapper>
+  );
 };
 
 const spin = keyframes`
   to { transform: rotate(360deg); }
 `;
-const size = 200;
+const size = 100;
 
 const S = {
-    SpinnerWrapper : styled.div`
-        width: ${size}px;
-        height:${size}px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    `,
-    
+  SpinnerWrapper: styled.div`
+    width: 500px;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+  `,
 
-    Spinner : styled.div`
+  Spinner: styled.div`
     width: ${size}px;
     height: ${size}px;
-    border:  ${size/12}px solid #fff3eb;
+    border: ${size / 12}px solid #fff3eb;
     border-top-color: #ff9856;
     border-radius: 50%;
     animation: ${spin} 0.8s linear infinite;
-    box-sizing: border-box; 
-    `,
-}
+    box-sizing: border-box;
+  `,
+};
 export default Loading;
-
