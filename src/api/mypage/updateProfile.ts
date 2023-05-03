6cb8dbedc48591eb8683cadfe0f68b3b17dcedb5
김유나 <yuna000804@gmail.com>
2023-05-04 02:54:28 +0900

    feat: profile POST api 함수 작성

---
+++ src/api/mypage/updateProfile.ts
@@ -0,0 +1,36 @@
+import { ProfileUpdateType } from "@/src/types/mypage/profileType";
+import instance from "../axiosModule";
+
+const withFormData = (data: ProfileUpdateType) => {
+  const formData = new FormData();
+  Object.entries(data).forEach(([key, value]) => {
+    if (typeof value === "boolean") {
+      formData.append(key, value ? "true" : "false");
+    } else if (key === "image") {
+      if (value === null) {
+        formData.append(key, "null");
+        formData.delete("image");
+      } else {
+        console.log("image");
+        formData.append(key, value.get("image"));
+      }
+    } else if (typeof value === "string") {
+      formData.append(key, value);
+    }
+  });
+  return formData;
+};
+
+export const postUpdateProfile = async (
+  profileUpdateData: ProfileUpdateType
+) => {
+  const response = await instance.post(
+    "/profile",
+    withFormData(profileUpdateData),
+    {
+      headers: { "Content-Type": "multipart/form-data" },
+    }
+  );
+
+  return response.data;
+};
