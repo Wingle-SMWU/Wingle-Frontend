25de89397a97dd8f307778928bdc53ddae9459f7
yeonkr <1714882@sookmyung.ac.kr>
2023-05-04 08:50:00 +0900

    fix: 쪽지함 profiledefault 유저 이미지 수정

--- src/components/messageComp/msgList.tsx
+++ src/components/messageComp/msgList.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import { Room } from "@/src/types/message/roomType";
 import betweenTime from "@/src/utils/betweenTime";
+import Image from "next/image";
+
+import profiledefault from '../../../public/images/message/profiledefault.png';
 
 const MsgList = ({ list }: { list: Room }) => {
   const { image, nickname, createdTime, recentChat, roomId } = list;
@@ -14,7 +17,12 @@ const MsgList = ({ list }: { list: Room }) => {
     <>
       <S.Container onClick={handleMoveChatRoom}>
         <S.LeftBox>
-          <S.UserImage src={image} alt="상대 이미지" />
+        {image ? 
+          <S.UserImage src={image} alt="상대 이미지" /> : 
+          <Image 
+          src='/images/message/profiledefault.png'
+          alt='기본 프로필 이미지'
+          width={35} height={35} />}
           <S.LeftContent>
             <S.LeftDetail>
               <span>{nickname}</span>
@@ -47,7 +55,6 @@ const S = {
     width: 40px;
     height: 36px;
     border-radius: 50%;
-    border: 1px solid red;
   `,
 
   LeftContent: styled.div`
