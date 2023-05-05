e5acc33a420ade5f91db7d308de387e0cb7de22a
김유나 <yuna000804@gmail.com>
2023-05-05 14:06:58 +0900

    style: footer 위치 조정

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,28 +1,12 @@
 import styled from "styled-components";
 import { Margin, Text } from "../../ui";
 
-const Style = {
-  Wrapper: styled.div`
-    width: 500px;
-    height: 270px;
-    background-color: #fcfcfc;
-    border-top: 1px solid #eeeef2;
-  `,
-  Content: styled.div`
-    padding: 32px 24px;
-  `,
-  WingleLogo: styled.img`
-    width: 80px;
-    height: 30px;
-  `,
-};
-
 export default function Footer() {
   return (
     <>
-      <Style.Wrapper>
-        <Style.Content>
-          <Style.WingleLogo src="footer-logo.png" alt="wingle-logo" />
+      <S.Wrapper>
+        <S.Content>
+          <S.WingleLogo src="footer-logo.png" alt="wingle-logo" />
           <Margin direction="column" size={32} />
           <Text.Caption2 color="gray500">
             공식 노션 바로가기 | 윙글 SNS 바로가기
@@ -35,8 +19,26 @@ export default function Footer() {
           <Text.Caption2 color="gray500">
             이용약관 | 개인정보처리방침
           </Text.Caption2>
-        </Style.Content>
-      </Style.Wrapper>
+        </S.Content>
+      </S.Wrapper>
     </>
   );
 }
+
+const S = {
+  Wrapper: styled.div`
+    width: 500px;
+    height: 270px;
+    position: fixed;
+    bottom: 0;
+    background-color: #fcfcfc;
+    border-top: 1px solid #eeeef2;
+  `,
+  Content: styled.div`
+    padding: 32px 24px;
+  `,
+  WingleLogo: styled.img`
+    width: 80px;
+    height: 30px;
+  `,
+};
