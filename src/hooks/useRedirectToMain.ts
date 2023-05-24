1cac46ab35d740417de7a56cf438d284768c8438
김유나 <yuna000804@gmail.com>
2023-05-24 19:01:44 +0900

    fix: 페이지 이동 후 새로고침 추가

--- src/hooks/useRedirectToMain.ts
+++ src/hooks/useRedirectToMain.ts
@@ -4,17 +4,20 @@ import { getRefreshTokenFromLocalStorage } from "../utils/refreshTokenHandler";
 
 export const useRedirectToMain = () => {
   const router = useRouter();
-  console.log(router.pathname, router.query);
   useEffect(() => {
     if (
       router.pathname !== "/auth/login" &&
-      router.pathname !== "/auth/signup"
+      router.pathname !== "/auth/signup" &&
+      router.pathname !== "/admin"
     ) {
       const checkLogined = () => {
         const logined = getRefreshTokenFromLocalStorage();
         if (!logined) {
           alert("로그인이 필요합니다.");
           router.replace("/auth/login");
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
         }
       };
       checkLogined();
