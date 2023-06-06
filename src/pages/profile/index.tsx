8769e3d588dbe8510ed67100fa10645f6d963368
JudithHopps <kookjeayon100@gmail.com>
2023-06-06 15:52:00 +0900

    feat : 타인 프로필 페이지 추가

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,386 @@
+import Modal from "@/src/components/modal";
+import { Text, Margin } from "@/src/components/ui";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import Loading from "@/src/components/ui/loadingUI";
+import instance from "@/src/api/axiosModule";
+import { getImageUrl, countryImg } from "@/src/modules/utils";
+import { ProfileStateType } from "@/src/types/mypage/profileType";
+
+export default function Edit(): JSX.Element {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [profileData, setProfileData] = useState<ProfileStateType>();
+
+  const router = useRouter();
+  const userID = router.query["userID"];
+
+  const getProfile = async (): Promise<void> => {
+    try {
+      const res = await instance.get(`/profile/${userID}`);
+      setProfileData(res.data.data);
+      setIsLoading(false);
+    } catch (err) {
+      setIsError(true);
+      throw err;
+    }
+  };
+  useEffect(() => {
+    if (userID) getProfile();
+  }, [userID]);
+
+  const onClickModal = (): void => {
+    setModalVisible((prev) => !prev);
+  };
+
+  const sendNote = (): void => {
+    // 연주님이 추가 필요
+    // userID는 위에서 가져오면 돼요!
+    // router.push('/messages/~)
+  };
+  if (isLoading) return <Loading />;
+  if (isError) return <>에러</>;
+
+  return (
+    <>
+      <S.Wapper>
+        <S.Content>
+          <S.Header>
+            <S.GoBackArrow
+              src="/back-arrow.svg"
+              alt="뒤로가기"
+              onClick={(): Promise<boolean> => router.push(`/mypage`)}
+            />
+            <Margin direction="row" size={13} />
+            <Text.Title1 color="gray900">프로필</Text.Title1>
+          </S.Header>
+          <>
+            <S.UserBox>
+              <S.UserImgBox>
+                <S.UserProfileImg
+                  src={
+                    profileData &&
+                    (profileData.image
+                      ? profileData.image
+                      : getImageUrl("기본"))
+                  }
+                  alt="프로필"
+                />
+                <S.UserFlagImg
+                  src={profileData && countryImg(profileData.nation)}
+                  alt="국기"
+                />
+              </S.UserImgBox>
+              <S.UserInfoBox>
+                <S.UserNicknameAndSex>
+                  <Text.Body1 color="gray900">
+                    {profileData && profileData.nickname}
+                  </Text.Body1>
+                  <S.UserSexImg
+                    src={
+                      profileData && profileData.gender
+                        ? "/mypage/female.svg"
+                        : "/mypage/male.svg"
+                    }
+                    alt="성별"
+                  />
+                </S.UserNicknameAndSex>
+                <Text.Body6 color="gray800">
+                  {profileData && profileData.nation}
+                </Text.Body6>
+              </S.UserInfoBox>
+            </S.UserBox>
+          </>
+
+          {/* <S.EditList>
+            <Margin direction="column" size={32} />
+
+            <S.Column>
+              <S.Language>
+                <Text.Body1 color="gray900">사용언어</Text.Body1>
+                <S.EditBtn
+                  src="/modify.svg"
+                  alt="연필"
+                  onClick={(): Promise<boolean> =>
+                    router.push(`/mypage/edit/language`)
+                  }
+                />
+              </S.Language>
+              <S.LanguageContent>
+                {profileData &&
+                  profileData.languages.map((v) => (
+                    <S.LanguageChartContent key={v.order}>
+                      <S.LanguageChart src={`/mypage/language${v.order}.svg`} />
+                      <S.LanguageText fontWeight={550} width={28}>
+                        {v.interest.substring(0, 2)}{" "}
+                      </S.LanguageText>
+                      <S.LanguageText
+                        fontWeight={400}
+                        width={262}
+                        color="orange500"
+                      >
+                        {v.interest.substring(2)}
+                      </S.LanguageText>
+                    </S.LanguageChartContent>
+                  ))}
+              </S.LanguageContent>
+            </S.Column>
+
+            <Margin direction="column" size={32} />
+            <S.Column>
+              <S.Introduce>
+                <Text.Body1 color="gray900">자기소개</Text.Body1>
+                <S.EditBtn
+                  src="/modify.svg"
+                  alt="연필"
+                  onClick={(): Promise<boolean> =>
+                    router.push(`/mypage/edit/introduce`)
+                  }
+                />
+              </S.Introduce>
+              <S.IntroduceContent>
+                {profileData && profileData.introduce}
+              </S.IntroduceContent>
+            </S.Column>
+
+            <Margin direction="column" size={32} />
+            <S.Column>
+              <S.Interest>
+                <Text.Body1 color="gray900">관심사</Text.Body1>
+                <S.EditBtn
+                  src="/modify.svg"
+                  alt="연필"
+                  onClick={(): Promise<boolean> =>
+                    router.push(`/mypage/edit/interest`)
+                  }
+                />
+              </S.Interest>
+              <S.InterestBoxContainer>
+                {profileData &&
+                  profileData.interests.map((item, index) => {
+                    const isLastItemInRow = (index + 1) % 3 === 0;
+
+                    return (
+                      <S.ShowInterest key={item}>
+                        <S.InterestBox backgroundColor="#FFF3EB">
+                          <Text.Body6 color="gray900" pointer>
+                            {item}
+                          </Text.Body6>
+                        </S.InterestBox>
+                        {isLastItemInRow && (
+                          <Margin direction="column" size={0} />
+                        )}
+                      </S.ShowInterest>
+                    );
+                  })}
+              </S.InterestBoxContainer>
+            </S.Column>
+          </S.EditList> */}
+          <S.Note onClick={sendNote}>쪽지 보내기</S.Note>
+        </S.Content>
+        {modalVisible && (
+          <Modal type="profile-back" onClickModal={onClickModal} />
+        )}
+      </S.Wapper>
+    </>
+  );
+}
+
+interface IntesestBoxProps {
+  backgroundColor: string;
+}
+
+interface LanguageText {
+  fontWeight: number;
+  width: number;
+}
+const S = {
+  Wapper: styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    /* border: 1px solid red; */
+  `,
+  Content: styled.div`
+    padding: 0 24px;
+  `,
+  Header: styled.div`
+    display: flex;
+    padding: 14px 0;
+  `,
+  GoBackArrow: styled.img`
+    cursor: pointer;
+  `,
+
+  UserBox: styled.div`
+    display: flex;
+    align-items: center;
+    /* border-bottom: 1px solid #eeeef2; */
+    gap: 14px;
+    position: relative;
+  `,
+  UserImgBox: styled.div`
+    width: 56px;
+    height: 56px;
+    position: relative;
+  `,
+  UserProfileImg: styled.img`
+    width: 56px;
+    height: 56px;
+    position: absolute;
+    border-radius: 100px;
+    border: 1px solid #eeeef2;
+  `,
+  UserFlagImg: styled.img`
+    width: 22px;
+    height: 22px;
+    position: absolute;
+    border: 1px solid white;
+    background-color: white;
+    border-radius: 100px;
+    right: 0%;
+    bottom: 0%;
+    z-index: 1;
+  `,
+  UserInfoBox: styled.div`
+    width: 340px;
+    height: 86px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  `,
+  UserNicknameAndSex: styled.div`
+    display: flex;
+  `,
+  UserSexImg: styled.img`
+    width: 16px;
+    height: 16px;
+    padding-left: 4px;
+    margin: 3px 0px;
+  `,
+
+  DropBubbleHigh: styled.div`
+    position: absolute;
+    top: 63px;
+    left: 435px;
+    border-bottom: 8px solid #303033;
+    border-left: 4px solid transparent;
+    border-right: 4px solid transparent;
+  `,
+
+  DropBubbleLow: styled.div`
+    width: 153px;
+    height: 42px;
+    background-color: #303033;
+    border-radius: 8px;
+    position: absolute;
+    top: 70px;
+    left: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+
+  RegisterBtn: styled.button`
+    width: 45px;
+    height: 33px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ff812e;
+    border-radius: 8px;
+  `,
+
+  EditList: styled.div`
+    width: 452px;
+    display: flex;
+    flex-direction: column;
+  `,
+  Language: styled.div`
+    width: 452px;
+    display: flex;
+    justify-content: space-between;
+  `,
+  LanguageContent: styled.div`
+    margin-top: 16px;
+    padding: 16px, 24px, 24px, 24px;
+  `,
+  LanguageChartContent: styled.div`
+    display: flex;
+    flex-direction: row;
+    padding-bottom: 6px;
+  `,
+  LanguageChart: styled.img`
+    width: 18px;
+    height: 18px;
+    padding-right: 6px;
+  `,
+  LanguageText: styled.div<LanguageText>`
+    width: ${(props) => props.width}px;
+    color: #49494d;
+    font-size: 14px;
+    font-family: "Pretendard";
+    font-weight: ${(props) => props.fontWeight};
+  `,
+  Introduce: styled.div`
+    width: 452px;
+    display: flex;
+    justify-content: space-between;
+  `,
+  IntroduceContent: styled.div`
+    padding-top: 16px;
+    font-size: 16px;
+    line-height: 140%;
+    color: theme.color.gray900;
+  `,
+  InterestBoxContainer: styled.div`
+    width: 80%;
+  `,
+  Interest: styled.div`
+    width: 452px;
+    display: flex;
+    justify-content: space-between;
+  `,
+  ShowInterest: styled.div`
+    display: inline-flex;
+    flex-direction: column;
+  `,
+  InterestBox: styled.div<IntesestBoxProps>`
+    cursor: pointer;
+    border-radius: 40px;
+    padding: 8px 15px;
+    display: inline-flex;
+    background-color: ${(props) => props.backgroundColor};
+    margin: 8px;
+  `,
+
+  Column: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+  Note: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 17px 16px;
+    gap: 6px;
+    position: fixed;
+    width: 404px;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 32px;
+
+    /* main_orange/orange500 */
+    background: #ff812e;
+    border-radius: 8px;
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    color: #ffffff;
+  `,
+};
