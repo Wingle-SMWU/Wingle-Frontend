45d38c68e4fd3e15beff80c865fdbf16cf645fb0
Rhino-ty <okko8522@gmail.com>
2023-06-06 22:45:04 +0900

    #70 - refactor: emailVerify Return 타입 설정

--- src/components/authpage/signup/signUpInput/emailVerify.tsx
+++ src/components/authpage/signup/signUpInput/emailVerify.tsx
@@ -1,6 +1,6 @@
 import { signUpFormDataAtom } from "@/src/atoms/auth/signUpAtoms";
 import { Margin } from "@/src/components/ui";
-import { useCallback, useState } from "react";
+import { ChangeEvent, useCallback, useState } from "react";
 import { useSetRecoilState } from "recoil";
 
 import {
@@ -9,8 +9,10 @@ import {
 } from "@/src/api/auth/emailAPI";
 import { useMutation } from "react-query";
 import TextInputWithButton from "@/src/components/ui/textInputWithButton";
+import { EmailAuthResponse } from "@/src/types/auth/emailApiType";
+import { SignUpFormData } from "@/src/types/auth/signupFormDataType";
 
-export default function EmailVerify() {
+export default function EmailVerify(): JSX.Element {
   const [buttonMessage, setButtonMessage] = useState("인증 전송");
   const [emailMent, setEmailMent] = useState("");
   const [emailCertificationMent, setEmailCertificationMent] = useState("");
@@ -28,18 +30,18 @@ export default function EmailVerify() {
     useState(true);
 
   const handleEmailInputData = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value),
+    (e: React.ChangeEvent<HTMLInputElement>): void => setEmail(e.target.value),
     []
   );
   const handleEmailertificationInputData = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) =>
+    (e: React.ChangeEvent<HTMLInputElement>): void =>
       setEmailCertification(e.target.value),
     []
   );
 
   // 이메일 유효성 검사
   const handleErrorEmail = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
       const emailRegex = /^[A-Za-z0-9]+@[^\s@]+\.[^\s@]+$/; // 영어와 숫자만 사용 가능한 정규식
       const value = e.target.value;
       const hasValidFormat = emailRegex.test(value);
@@ -60,7 +62,7 @@ export default function EmailVerify() {
 
   // 이메일 인증번호 유효성 검사
   const handleErrorEmailCertify = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
       const emailRegex = /^\d+$/;
       const value = e.target.value;
       const hasValidFormat = emailRegex.test(value);
@@ -76,22 +78,25 @@ export default function EmailVerify() {
   );
 
   // 이메일 인증메일 보내기
-  const { mutate: sendEmail } = useMutation(() => sendEmailAuth(email), {
-    onMutate: () => {
-      setButtonMessage("전송 중");
-    },
-    onSuccess: () => {
-      setButtonMessage("재전송");
-      setEmailMent("인증메일을 전송했습니다.");
-    },
-    onError: (error) => {
-      setErrorEmailCertify(true);
-      alert(error);
-      throw error;
-    },
-  });
+  const { mutate: sendEmail } = useMutation(
+    (): Promise<EmailAuthResponse> => sendEmailAuth(email),
+    {
+      onMutate: (): void => {
+        setButtonMessage("전송 중");
+      },
+      onSuccess: (): void => {
+        setButtonMessage("재전송");
+        setEmailMent("인증메일을 전송했습니다.");
+      },
+      onError: (error: unknown): never => {
+        setErrorEmailCertify(true);
+        alert(error);
+        throw error;
+      },
+    }
+  );
 
-  const handleSendEmail = useCallback(() => {
+  const handleSendEmail = useCallback((): void => {
     if (email === "") {
       alert("이메일을 입력해주세요.");
       return;
@@ -101,24 +106,27 @@ export default function EmailVerify() {
 
   // 이메일 인증번호 확인
   const { mutate: verifyEmail } = useMutation(
-    () => verifyEmailCertification({ email, emailCertification }),
+    (): Promise<boolean> =>
+      verifyEmailCertification({ email, emailCertification }),
     {
-      onSuccess: () => {
+      onSuccess: (): void => {
         setErrorEmailCertify(false);
-        setSignUpFormData((prev) => ({
-          ...prev,
-          email,
-        }));
+        setSignUpFormData(
+          (prev: SignUpFormData): SignUpFormData => ({
+            ...prev,
+            email,
+          })
+        );
         setEmailCertificationMent("인증번호가 일치합니다.");
       },
-      onError: (error) => {
+      onError: (error: unknown): never => {
         setErrorEmailCertify(true);
         throw error;
       },
     }
   );
 
-  const handleVerifyEmail = useCallback(() => {
+  const handleVerifyEmail = useCallback((): void => {
     if (email === "") {
       alert("이메일을 입력해주세요.");
       return;
@@ -133,7 +141,7 @@ export default function EmailVerify() {
         name="이메일"
         placeholder="abc@naver.com"
         value={email}
-        onChange={(e) => {
+        onChange={(e: ChangeEvent<HTMLInputElement>): void => {
           handleEmailInputData(e);
           handleErrorEmail(e);
         }}
@@ -151,7 +159,7 @@ export default function EmailVerify() {
         name="이메일 인증"
         placeholder="인증번호"
         value={emailCertification}
-        onChange={(e) => {
+        onChange={(e: ChangeEvent<HTMLInputElement>): void => {
           handleEmailertificationInputData(e);
           handleErrorEmailCertify(e);
         }}
