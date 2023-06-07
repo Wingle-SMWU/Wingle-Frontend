268ace3f84b5dd22614b14a34d034aefd089f876
Rhino-ty <okko8522@gmail.com>
2023-06-07 22:25:11 +0900

    #70 - design: StudentCard 디자인 수정 및 벡터이미지 색상 수정

--- src/components/authpage/signup/studentCard.tsx
+++ src/components/authpage/signup/studentCard.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 import { useSetRecoilState } from "recoil";
 import { signUpFormDataAtom } from "@/src/atoms/auth/signUpAtoms";
 import { SignUpFormData } from "@/src/types/auth/signupFormDataType";
+import { theme } from "@/src/styles/theme";
 
-export default function StudentCard() {
+export default function StudentCard(): JSX.Element {
   const [isActive, setIsActive] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
   const setSignUpFormData = useSetRecoilState(signUpFormDataAtom);
@@ -14,32 +15,38 @@ export default function StudentCard() {
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   // TODO: 이미지 업로드 시 MultipartFile로 변환해서 보내야 함
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileUpload = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
     const imageFile = event.target.files?.[0];
     if (imageFile) {
       const fileSizeInMB = imageFile.size / (1024 * 1024);
       if (fileSizeInMB > 20) {
         // 20MB 이하인 경우에만 처리
         setError(true);
-        setSignUpFormData((prev: SignUpFormData) => ({
-          ...prev,
-          idCardImage: null, // null 로 초기화
-        }));
+        setSignUpFormData(
+          (prev: SignUpFormData): SignUpFormData => ({
+            ...prev,
+            idCardImage: null, // null 로 초기화
+          })
+        );
         return;
       }
       setError(false);
       const formData = new FormData();
       formData.append("idCardImage", imageFile);
       // MultipartFile 데이터를 formData 에 append
-      setSignUpFormData((prev: SignUpFormData) => ({
-        ...prev,
-        idCardImage: formData,
-      }));
+      setSignUpFormData(
+        (prev: SignUpFormData): SignUpFormData => ({
+          ...prev,
+          idCardImage: formData,
+        })
+      );
       setUploadedFileName(imageFile.name);
     }
   };
 
-  const handleUploadButtonClick = () => {
+  const handleUploadButtonClick = (): void => {
     fileInputRef.current?.click();
   };
 
@@ -50,8 +57,8 @@ export default function StudentCard() {
         <S.QuestionLogo
           src="/auth/question.svg"
           alt="question"
-          onClick={() => {
-            setIsActive((prev) => !prev);
+          onClick={(): void => {
+            setIsActive((prev: boolean): boolean => !prev);
           }}
         ></S.QuestionLogo>
       </Text.Title1>
@@ -62,8 +69,8 @@ export default function StudentCard() {
           <Text.Body5 color="gray100">학생증 인증 방법</Text.Body5>
           <Margin direction="column" size={8} />
           <Text.Body6 color="gray100">
-            카드 학생증 앞면/모바일 학생증 캡처본/숙명포털-학적사항 중 한 가지를 첨부해주세요.
-            (이름, 학과, 학번이 정확히 나와야 합니다.)
+            카드 학생증 앞면/모바일 학생증 캡처본/숙명포털-학적사항 중 한 가지를
+            첨부해주세요. (이름, 학과, 학번이 정확히 나와야 합니다.)
           </Text.Body6>
         </S.Description>
       </S.DescriptionContent>
@@ -76,8 +83,13 @@ export default function StudentCard() {
           onChange={handleFileUpload}
           style={{ display: "none" }}
         />
-        <S.UploadLogo src="/auth/upload.svg" alt="upload" width={24} height={24} />
-        <Text.Body1 color="gray700">학생증 업로드</Text.Body1>
+        <S.UploadLogo
+          src="/auth/upload.svg"
+          alt="upload"
+          width={24}
+          height={24}
+        />
+        <Text.Body1 color="white">학생증 업로드</Text.Body1>
       </S.UploadButton>
       <Margin direction="column" size={8} />
 
@@ -85,16 +97,20 @@ export default function StudentCard() {
         <S.ErrorWrapper>
           <Image src="/auth/error.svg" alt="question" width={16} height={16} />
           <Margin direction="row" size={8} />
-          <Text.Caption3 color="red500">파일 업로드를 실패했습니다</Text.Caption3>
+          <Text.Caption3 color="red500">
+            파일 업로드를 실패했습니다
+          </Text.Caption3>
         </S.ErrorWrapper>
       ) : uploadedFileName ? (
         <S.FileName color="gray500" onClick={handleUploadButtonClick}>
           {uploadedFileName}
         </S.FileName>
       ) : (
-        <Text.Caption3 color="gray500">20MB 이하 파일을 업로드해주세요.</Text.Caption3>
+        <Text.Caption3 color="gray500">
+          20MB 이하 파일을 업로드해주세요.
+        </Text.Caption3>
       )}
-      <Margin direction="column" size={24} />
+      <Margin direction="column" size={52} />
     </S.CertifyWrapper>
   );
 }
@@ -110,7 +126,8 @@ const S = {
   `,
 
   DescriptionContent: styled.div<{ isActive: boolean }>`
-    display: ${(props) => (props.isActive ? `block` : `none`)};
+    display: ${(props): "none" | "block" =>
+      props.isActive ? `block` : `none`};
     position: absolute;
     width: 452px;
     height: 100px;
@@ -123,19 +140,17 @@ const S = {
   ErrorWrapper: styled.div`
     display: flex;
   `,
+
   UploadButton: styled.button`
     width: 452px;
-    border: 1px solid #6c6c70;
-    outline: none;
     height: 52px;
+    background-color: ${theme.color.orange500};
+    outline: none;
     border-radius: 8px;
     display: flex;
     justify-content: center;
     padding: 15px;
     cursor: pointer;
-    &:focus {
-      border: 1px solid #6c6c70;
-    }
 
     span {
       cursor: pointer;
@@ -146,6 +161,5 @@ const S = {
   `,
   FileName: styled(Text.Caption3)`
     cursor: pointer;
-    text-decoration: underline;
   `,
 };
