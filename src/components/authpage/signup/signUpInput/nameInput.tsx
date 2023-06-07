d8a0f48cf81b4ef885359301c3cee9b9d11552b8
Rhino-ty <okko8522@gmail.com>
2023-06-07 21:27:14 +0900

    #70 - feat: NameInput에 TextInputUI 적용
    얘도 조건마다 멘트가 달라지기에 멘트 상태 추가해야함

--- src/components/authpage/signup/signUpInput/nameInput.tsx
+++ src/components/authpage/signup/signUpInput/nameInput.tsx
@@ -1,16 +1,12 @@
 import { signUpFormDataAtom } from "@/src/atoms/auth/signUpAtoms";
-import { Margin, Text } from "@/src/components/ui";
-import { useCallback, useEffect, useState } from "react";
+import { Margin } from "@/src/components/ui";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
-import styled from "styled-components";
-import { ErrorMent } from "../errorMent";
 
-interface StyledInputProps {
-  small: boolean;
-  error: boolean;
-}
+import TextInputUI from "@/src/components/ui/textInputUI";
+import { SignUpFormData } from "@/src/types/auth/signupFormDataType";
 
-export default function NameInput() {
+export default function NameInput(): JSX.Element {
   const [nameInputData, setNameInputData] = useState("");
 
   const setSignUpFormData = useSetRecoilState(signUpFormDataAtom);
@@ -18,17 +14,19 @@ export default function NameInput() {
   const [isErrorName, setErrorName] = useState(true);
 
   // useEffect로 이름 존재 시 회원가입 폼 데이터 저장
-  useEffect(() => {
+  useEffect((): void => {
     if (!isErrorName) {
-      setSignUpFormData((prev) => ({
-        ...prev,
-        name: nameInputData,
-      }));
+      setSignUpFormData(
+        (prev: SignUpFormData): SignUpFormData => ({
+          ...prev,
+          name: nameInputData,
+        })
+      );
     }
   }, [isErrorName, nameInputData, setSignUpFormData]);
 
-  const handleInputData = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleNameInputData = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
       setNameInputData(e.target.value);
     },
     []
@@ -36,7 +34,7 @@ export default function NameInput() {
 
   // 이름 유효성 검사
   const handleErrorName = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
       const special_pattern = /^[a-zA-Z가-힣\s]+$/;
       if (!special_pattern.test(e.target.value)) {
         setErrorName(true);
@@ -49,74 +47,19 @@ export default function NameInput() {
 
   return (
     <>
-      <Text.Body1 color="gray700">이름</Text.Body1>
-      <Margin direction="column" size={8} />
-      <S.ContentWrapper>
-        <S.Content>
-          <S.InputField small={false} error={isErrorName}>
-            <input
-              name="name"
-              value={nameInputData}
-              type="string"
-              placeholder="김윙글"
-              onChange={(e) => {
-                handleInputData(e);
-                handleErrorName(e);
-              }}
-            />
-          </S.InputField>
-        </S.Content>
-        <ErrorMent
-          error={isErrorName}
-          errorMent="실명을 입력하세요 (한글, 영어 대/소문자 사용 가능) "
-          ment=" 실명을 입력하세요 (한글, 영어 대/소문자 사용 가능) "
-        />
-      </S.ContentWrapper>
+      <TextInputUI
+        label="이름"
+        name="이름"
+        value={nameInputData}
+        onChange={(e: ChangeEvent<HTMLInputElement>): void => {
+          handleNameInputData(e);
+          handleErrorName(e);
+        }}
+        placeholder="실명을 입력하세요"
+        error={isErrorName}
+        errorMessage="한글 또는 영문으로 입력해주세요. "
+      />
+      <Margin direction="column" size={24} />
     </>
   );
 }
-
-const S = {
-  ContentWrapper: styled.div`
-    padding-bottom: 24px;
-  `,
-  Content: styled.div`
-    display: flex;
-  `,
-  InputField: styled.div<StyledInputProps>`
-    height: 50px;
-    border: ${(props) =>
-      props.error ? "1px solid #FF7070" : "1px solid #dcdce0;"};
-    border-radius: 8px;
-    margin-bottom: 8px;
-
-    & > input {
-      width: ${(props) => (props.small ? "312px" : "392px")};
-      border: none;
-      padding: 14px;
-      border-radius: 8px;
-      height: 22px;
-
-      &::placeholder {
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 140%;
-        color: #959599;
-      }
-    }
-  `,
-  Button: styled.button`
-    font-size: 16px;
-    font-weight: 700;
-    color: #49494d;
-    width: 99px;
-  `,
-  ButtonWrapper: styled.div<StyledInputProps>`
-    display: ${(props) => (props.small ? "flex" : "none")};
-    height: 50px;
-    width: 99px;
-    border: 1px solid #959599;
-    border-radius: 8px;
-    margin-left: 8px;
-  `,
-};
