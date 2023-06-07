086665af68aea4a3ab05277b223cafdc52a3c0c0
Rhino-ty <okko8522@gmail.com>
2023-06-07 21:55:48 +0900

    #70 - feat: NicknameVerify 사용안하는 코드 삭제 및 에러멘트 상태 추가
    에러도 2가지 상황으로 나뉘어 상태 추가했음. 각 상황에 맞게 setter 배치

--- src/components/authpage/signup/signUpInput/nicknameVerify.tsx
+++ src/components/authpage/signup/signUpInput/nicknameVerify.tsx
@@ -1,35 +1,32 @@
 import { signUpFormDataAtom } from "@/src/atoms/auth/signUpAtoms";
-import { Margin, Text } from "@/src/components/ui";
+import { Margin } from "@/src/components/ui";
 import { ChangeEvent, useCallback, useState } from "react";
 import { useSetRecoilState } from "recoil";
-import styled from "styled-components";
-import { ErrorMent } from "../errorMent";
 import { checkNicknameAvailable } from "@/src/api/auth/emailAPI";
 import { useMutation } from "react-query";
 import { EmailAuthResponse } from "@/src/types/auth/emailApiType";
 import { SignUpFormData } from "@/src/types/auth/signupFormDataType";
 import TextInputWithButton from "@/src/components/ui/textInputWithButton";
 
-interface StyledInputProps {
-  small: boolean;
-  error: boolean;
-}
-
 export default function NicknameVerify(): JSX.Element {
   const [nicknameInputData, setNicknameInputData] = useState("");
 
   const setSignUpFormData = useSetRecoilState(signUpFormDataAtom);
 
-  const [isErrorNickName, setErrorNickName] = useState(true);
-  const [isCheckedNickname, setCheckedNickname] = useState(false);
-  const [isVerifiedNickname, setVerifiedNickname] = useState(false);
+  const [isErrorNickName, setErrorNickName] = useState(false);
   const [isDisabledDoubleCheckButton, setDisabledDoubleCheckButton] =
     useState(true);
 
-  const [nicknameMent, setNicknameMent] = useState("");
+  const [nicknameMent, setNicknameMent] = useState(
+    "한글/영문/숫자 2자 이상 10자 미만"
+  );
+  const [nicknameErrorMent, setNicknameErrorMent] = useState(
+    "한글/영문/숫자 2자 이상 10자 미만"
+  );
 
   const handleNicknameInputData = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>): void => {
+      setNicknameErrorMent("한글/영문/숫자 2자 이상 10자 미만");
       setNicknameInputData(e.target.value);
     },
     []
@@ -47,10 +44,10 @@ export default function NicknameVerify(): JSX.Element {
         e.target.value.length > 10
       ) {
         setErrorNickName(true);
-        setDisabledDoubleCheckButton(false);
+        setDisabledDoubleCheckButton(true);
       } else {
         setErrorNickName(false);
-        setDisabledDoubleCheckButton(true);
+        setDisabledDoubleCheckButton(false);
       }
     },
     []
@@ -61,8 +58,7 @@ export default function NicknameVerify(): JSX.Element {
     (): Promise<EmailAuthResponse> => checkNicknameAvailable(nicknameInputData),
     {
       onSuccess: (): void => {
-        setCheckedNickname(true);
-        setVerifiedNickname(true);
+        setNicknameMent("사용 가능한 닉네임입니다.");
         setSignUpFormData(
           (prev: SignUpFormData): SignUpFormData => ({
             ...prev,
@@ -72,8 +68,10 @@ export default function NicknameVerify(): JSX.Element {
         );
       },
       onError: (error: unknown): never => {
-        setCheckedNickname(true);
-        setVerifiedNickname(false);
+        setNicknameMent("한글/영문/숫자 2자 이상 10자 미만");
+        setNicknameErrorMent("이미 사용중인 닉네임입니다.");
+        setErrorNickName(true);
+        setDisabledDoubleCheckButton(true);
         setSignUpFormData(
           (prev: SignUpFormData): SignUpFormData => ({
             ...prev,
@@ -103,101 +101,16 @@ export default function NicknameVerify(): JSX.Element {
         onChange={(e: ChangeEvent<HTMLInputElement>): void => {
           handleNicknameInputData(e);
           handleErrorNickName(e);
-          setVerifiedNickname(false);
-          setCheckedNickname(false);
         }}
         placeholder="실명을 입력하세요"
         error={isErrorNickName}
-        errorMessage="한글 또는 영문으로 입력해주세요."
+        errorMessage={nicknameErrorMent}
         buttonMessage="중복 확인"
         buttonDisabled={isDisabledDoubleCheckButton}
         onClick={handleCheckNickname}
         description={nicknameMent}
       />
       <Margin direction="column" size={24} />
-
-      <Text.Body1 color="gray700">닉네임</Text.Body1>
-      <Margin direction="column" size={8} />
-      <S.ContentWrapper>
-        <S.Content>
-          <S.InputField small={true} error={isErrorNickName}>
-            <input
-              name="nickname"
-              value={inputNicknameData}
-              type="string"
-              placeholder="희망찬윙그리"
-              onChange={(e: ChangeEvent<HTMLInputElement>): void => {
-                handleInputData(e);
-                handleErrorNickName(e);
-                setVerifiedNickname(false);
-                setCheckedNickname(false);
-              }}
-            />
-          </S.InputField>
-          <S.ButtonWrapper small={true} error={false}>
-            <S.Button onClick={(): void => handleCheckNickname()}>
-              중복 확인
-            </S.Button>
-          </S.ButtonWrapper>
-        </S.Content>
-        <ErrorMent
-          error={isErrorNickName}
-          errorMent="한글/영어 두글자 이상 10글자 이하로 입력해주세요."
-          ment="  "
-        />
-        {isCheckedNickname ? (
-          <ErrorMent
-            error={!isVerifiedNickname}
-            errorMent="이미 사용중인 닉네임입니다."
-            ment="사용가능한 닉네임입니다."
-          />
-        ) : null}
-      </S.ContentWrapper>
     </>
   );
 }
-
-const S = {
-  ContentWrapper: styled.div`
-    padding-bottom: 24px;
-  `,
-  Content: styled.div`
-    display: flex;
-  `,
-  InputField: styled.div<StyledInputProps>`
-    height: 50px;
-    border: ${(props) =>
-      props.error ? "1px solid #FF7070" : "1px solid #dcdce0;"};
-    border-radius: 8px;
-    margin-bottom: 8px;
-
-    & > input {
-      width: ${(props) => (props.small ? "312px" : "392px")};
-      border: none;
-      padding: 14px;
-      border-radius: 8px;
-      height: 22px;
-
-      &::placeholder {
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 140%;
-        color: #959599;
-      }
-    }
-  `,
-  Button: styled.button`
-    font-size: 16px;
-    font-weight: 700;
-    color: #49494d;
-    width: 99px;
-  `,
-  ButtonWrapper: styled.div<StyledInputProps>`
-    display: ${(props) => (props.small ? "flex" : "none")};
-    height: 50px;
-    width: 99px;
-    border: 1px solid #959599;
-    border-radius: 8px;
-    margin-left: 8px;
-  `,
-};
