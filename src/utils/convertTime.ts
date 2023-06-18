2f1481a37908f2c7a105f957f3b2e85cc4e2c613
JudithHopps <kookjeayon100@gmail.com>
2023-06-18 16:31:53 +0900

    Create a release branch

--- src/utils/convertTime.ts
+++ src/utils/convertTime.ts
@@ -1,4 +1,4 @@
-export const convertTime = (data: string) => {
+export const convertTime = (data: string): string => {
   const newDate = new Date(data).toTimeString().split(" ");
   const time = newDate[0].split(":").slice(0, 2).join(":");
   return time;
