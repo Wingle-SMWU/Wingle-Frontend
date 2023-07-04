c79e5808c80435c41db80f39be172ab344e49a9f
Rhino-ty <okko8522@gmail.com>
2023-07-04 23:47:29 +0900

    feat: pretendard npm 패키지 삭제 및 link로 cdn 불러오기
    그 전엔 npm 패키지로 깔려 있었는데, 글로벌 스타일에 불러오지 않아 글꼴이 적용 되지 않았음;; 용량이 적은 dynamic 글꼴로 cdn 불러와 글꼴 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link
+            rel="stylesheet"
+            as="style"
+            crossOrigin="anonymous"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard-dynamic-subset.css"
+          />
+          <link
+            rel="stylesheet"
+            as="style"
+            crossOrigin="anonymous"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
