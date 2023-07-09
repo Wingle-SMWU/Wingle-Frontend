98dc95de7acc8f67af14c98697391aebd96239f7
JudithHopps <kookjeayon100@gmail.com>
2023-07-09 15:19:26 +0900

    feat : 드롭다운 선택 창 수정

--- src/components/mypage/selectLanguage.tsx
+++ src/components/mypage/selectLanguage.tsx
@@ -6,27 +6,50 @@ export default function SelectLanguageBox({
   getLanguageAtIndex,
   initialLanguage,
   idx,
+  preSelctArr,
 }: Props): JSX.Element {
   const [language, setLanguage] = useState(initialLanguage);
-
+  const [preSelected, setIsPreSelected] = useState(preSelctArr);
+  // const preSelected = preSelctArr;
+  const [isdisable, setDisabled] = useState(false);
   const handleChange = (selectedLanguage: string): void => {
     setLanguage(selectedLanguage);
   };
 
+  console.log(preSelctArr, "~~");
   useEffect(() => {
     setLanguage(initialLanguage);
   }, [initialLanguage]);
 
   useEffect(() => {
+    setIsPreSelected(preSelctArr);
+  }, [preSelctArr]);
+
+  useEffect(() => {
     getLanguageAtIndex(language, idx);
-  }, [language]);
+    if (
+      idx !== 0 &&
+      // (preSelctArr[idx - 1] === undefined || preSelctArr[idx - 1] === "")
+      (preSelected[idx - 1] === undefined || preSelected[idx - 1] === "")
+    ) {
+      setDisabled(true);
+    }
+    console.log(
+      "idx 이전 선택 : ",
+      idx,
+      preSelctArr,
+      preSelctArr[idx - 1],
+      "~~"
+    );
+  }, [language, isdisable]);
 
   return (
     <DropDownCommon
-      list={countryList.map((country) => country.add_code_nation)}
-      selected={language}
+      list={countryList.map((country) => country.enNation)}
+      selected={language.length ? language : "언어선택"}
       handleSelectedChange={handleChange}
       dropDownPlaceHolder={language}
+      disabled={isdisable}
     />
   );
 }
@@ -35,4 +58,5 @@ type Props = {
   getLanguageAtIndex: (str: string, idx: number) => void;
   initialLanguage: string;
   idx: number;
+  preSelctArr: string[];
 };
