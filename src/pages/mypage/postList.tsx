5a5c5ec73eadbefc0be94b84aaaaee16f0b45c3f
김유나 <yuna000804@gmail.com>
2023-07-09 17:50:31 +0900

    fix: 내가 쓴 게시글 첫 진입 시 자유 탭 리턴

--- src/pages/mypage/postList.tsx
+++ src/pages/mypage/postList.tsx
@@ -1,5 +1,4 @@
 import { getForums } from "@/src/api/community/get/forums";
-import { currentTabStateAtom } from "@/src/atoms/community/tab";
 import FreeTab from "@/src/components/community/list/freeTab";
 import Header from "@/src/components/community/list/header";
 import InteractTab from "@/src/components/community/list/interactTab";
@@ -8,22 +7,18 @@ import { getImageUrl } from "@/src/modules/utils";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
 import { useQuery } from "react-query";
-import { useRecoilState } from "recoil";
 import styled from "styled-components";
 
 export default function PostList() {
   const router = useRouter();
-  // const currentTab: string = useMemo(() => {
-  //   if (!router.query.tab) {
-  //     return "자유";
-  //   }
-  //   return String(router.query.tab);
-  // }, [router.query.tab]);
-
-  const [currentTab, setCurrentTab] = useRecoilState(currentTabStateAtom);
+  const currentTab: string = useMemo(() => {
+    if (!router.query.tab) {
+      return "자유";
+    }
+    return String(router.query.tab);
+  }, [router.query.tab]);
 
   const onClickTab = (event: any) => {
-    setCurrentTab(event.target.textContent);
     router.push({ query: { tab: event.target.textContent } });
   };
 
