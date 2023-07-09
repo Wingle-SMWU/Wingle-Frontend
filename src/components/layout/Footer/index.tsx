1c3f27e203011a81d731b397457faa2e2d5a0824
김유나 <yuna000804@gmail.com>
2023-07-09 18:53:42 +0900

    add: Footer의 sns 링크 연결

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { Margin, Text } from "../../ui";
+import Link from "next/link";
+import { theme } from "@/src/styles/theme";
 
 export default function Footer() {
   return (
@@ -9,11 +11,23 @@ export default function Footer() {
           <S.WingleLogo src="footer-logo.png" alt="wingle-logo" />
           <Margin direction="column" size={32} />
           <Text.Caption2 color="gray500">
-            공식 노션 바로가기 | 윙글 SNS 바로가기
+            공식 노션 바로가기 |&nbsp;
+            <Link
+              href="https://www.instagram.com/wingle_official"
+              style={{ textDecoration: "none", color: theme.color.gray500 }}
+            >
+              윙글 SNS 바로가기
+            </Link>
           </Text.Caption2>
           <Margin direction="column" size={11} />
           <Text.Caption2 color="gray500">
-            문의 | wingle.kr@gmail.com
+            문의 |&nbsp;
+            <Link
+              href="mailto:wingle.kr@gmail.com"
+              style={{ textDecoration: "none", color: theme.color.gray500 }}
+            >
+              wingle.kr@gmail.com
+            </Link>
           </Text.Caption2>
           <Margin direction="column" size={11} />
           <Text.Caption2 color="gray500">
