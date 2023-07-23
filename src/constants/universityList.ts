3cf18c9c6b3b2ca1c7b69976d0fcef8d4c1c1d64
Rhino-ty <okko8522@gmail.com>
2023-07-23 14:52:55 +0900

    feat: 대학교 리스트 contant 추가

---
+++ src/constants/universityList.ts
@@ -0,0 +1,162 @@
+export type UniversityListType = {
+  code: string;
+  university: string;
+};
+export const universityList: UniversityListType[] = [
+  {
+    code: "001",
+    university: "가톨릭대",
+  },
+  {
+    code: "002",
+    university: "강서대",
+  },
+  {
+    code: "003",
+    university: "건국대(서울)",
+  },
+  {
+    code: "004",
+    university: "경기대",
+  },
+  {
+    code: "005A",
+    university: "경희대(서울)",
+  },
+  {
+    code: "005B",
+    university: "경희대(국제)",
+  },
+  {
+    code: "006",
+    university: "고려대(서울)",
+  },
+  {
+    code: "007",
+    university: "광운대",
+  },
+  {
+    code: "008",
+    university: "국민대",
+  },
+  {
+    code: "009",
+    university: "덕성여대",
+  },
+  {
+    code: "010",
+    university: "동국대",
+  },
+  {
+    code: "011",
+    university: "동덕여대",
+  },
+  {
+    code: "012A",
+    university: "명지대(인문)",
+  },
+  {
+    code: "012B",
+    university: "명지대(자연)",
+  },
+  {
+    code: "013",
+    university: "삼육대",
+  },
+  {
+    code: "014",
+    university: "상명대",
+  },
+  {
+    code: "015",
+    university: "서강대",
+  },
+  {
+    code: "016",
+    university: "서경대",
+  },
+  {
+    code: "017",
+    university: "서울과기대",
+  },
+  {
+    code: "018",
+    university: "서울대",
+  },
+  {
+    code: "019",
+    university: "서울시립대",
+  },
+  {
+    code: "020",
+    university: "서울여대",
+  },
+  {
+    code: "021",
+    university: "성공회대",
+  },
+  {
+    code: "022A",
+    university: "성균관대(인문)",
+  },
+  {
+    code: "022B",
+    university: "성균관대(자연)",
+  },
+  {
+    code: "023",
+    university: "성신여대",
+  },
+  {
+    code: "024",
+    university: "세종대",
+  },
+  {
+    code: "025",
+    university: "숙명여대",
+  },
+  {
+    code: "026",
+    university: "숭실대",
+  },
+  {
+    code: "027",
+    university: "연세대(신촌)",
+  },
+  {
+    code: "028",
+    university: "이화여대",
+  },
+  {
+    code: "029",
+    university: "중앙대(서울)",
+  },
+  {
+    code: "030",
+    university: "총신대",
+  },
+  {
+    code: "031",
+    university: "추계예대",
+  },
+  {
+    code: "032",
+    university: "한국외대(서울)",
+  },
+  {
+    code: "033",
+    university: "한국체대",
+  },
+  {
+    code: "034",
+    university: "한성대",
+  },
+  {
+    code: "035",
+    university: "한양대(서울)",
+  },
+  {
+    code: "036",
+    university: "홍익대(서울)",
+  },
+];
