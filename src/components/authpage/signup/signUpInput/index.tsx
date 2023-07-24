e19aaf976e839b7275165d77a9b0c8bbde2ca4c1
김유나 <yuna000804@gmail.com>
2023-07-24 17:16:50 +0900

    fix: 컴포넌트에서 사용한 getstaticprops 삭제

--- src/components/authpage/signup/signUpInput/index.tsx
+++ src/components/authpage/signup/signUpInput/index.tsx
@@ -4,8 +4,6 @@ import PasswordVerify from "./passwordVerify";
 import NameInput from "./nameInput";
 import NicknameVerify from "./nicknameVerify";
 import InputUnivDropdown from "./inputUnivDropdown";
-import { GetStaticProps } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function InputBox(): JSX.Element {
   return (
