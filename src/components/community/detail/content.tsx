7287b63f379cd5831fa7943aaa4d05e03ce83f3e
김유나 <yuna000804@gmail.com>
2023-07-25 17:28:17 +0900

    feat: 커뮤니티 공지게시판 html 태그 스타일 반영

--- src/components/community/detail/content.tsx
+++ src/components/community/detail/content.tsx
@@ -8,22 +8,42 @@ export const Content = ({
   text: string;
   isNotice: boolean;
   isDetail: boolean;
-}): JSX.Element => {
-  if (isNotice && text.includes("</h2>")) {
-    const title = text.replace(/<[^>]*>?/g, "");
+}) => {
+  const hasH2Tag = text.includes("</h2>") && isDetail;
+  const hasH3Tag = text.includes("</h3>") && isDetail;
+  const hasH4Tag = text.includes("</h4>") && isDetail;
+  const hasBoldTag = text.includes("</b>") && isDetail;
+  const title = text.replace(/<[^>]*>?/g, "");
+
+  // Function to render text with <br> if isDetail is true
+  const renderTextWithBr = (content: JSX.Element) => {
     return isDetail ? (
-      <Text.Title3 color="gray900">{title}</Text.Title3>
+      <>
+        {content}
+        <br />
+      </>
     ) : (
-      <Text.Body2 color="gray900" pointer={true}>
-        {title}
-      </Text.Body2>
+      content
     );
+  };
+
+  if (isNotice) {
+    if (hasH2Tag) {
+      return <Text.Title3 color="gray900">{title}</Text.Title3>;
+    } else if (hasH3Tag || hasH4Tag || hasBoldTag) {
+      return renderTextWithBr(<Text.Body1 color="gray900">{title}</Text.Body1>);
+    } else {
+      return renderTextWithBr(
+        <Text.Body2 color="gray900" pointer={!isDetail}>
+          {title}
+        </Text.Body2>
+      );
+    }
   } else {
     return (
-      <div>
-        {text}
-        {isDetail && <br />}
-      </div>
+      <>
+        <div>{text}</div>
+      </>
     );
   }
 };
